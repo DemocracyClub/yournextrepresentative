1f4bff6da36da5febf17e907a4dbc27b206a69ed
octopusinvitro <octopusinvitro@users.noreply.github.com>
2017-05-05 16:34:58 +0100

    Add a data migration for a group that allows people to review changes

--- candidates/models/__init__.py
+++ candidates/models/__init__.py
@@ -34,4 +34,5 @@ from .db import ChangeReviewed
 from .auth import TRUSTED_TO_MERGE_GROUP_NAME
 from .auth import TRUSTED_TO_LOCK_GROUP_NAME
 from .auth import TRUSTED_TO_RENAME_GROUP_NAME
+from .auth import TRUSTED_TO_REVIEW_CHANGES_GROUP_NAME
 from .auth import RESULT_RECORDERS_GROUP_NAME
--- candidates/models/auth.py
+++ candidates/models/auth.py
@@ -8,6 +8,7 @@ from auth_helpers.views import user_in_group
 TRUSTED_TO_MERGE_GROUP_NAME = 'Trusted To Merge'
 TRUSTED_TO_LOCK_GROUP_NAME = 'Trusted To Lock'
 TRUSTED_TO_RENAME_GROUP_NAME = 'Trusted To Rename'
+TRUSTED_TO_REVIEW_CHANGES_GROUP_NAME = 'Trusted To Review Changes'
 RESULT_RECORDERS_GROUP_NAME = 'Result Recorders'
 
 class NameChangeDisallowedException(Exception):
