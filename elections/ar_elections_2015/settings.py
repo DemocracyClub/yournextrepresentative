b42e0205cf1ebcb0f31bfd440cea4cc8941849e2
Sym Roe <sym.roe@talusdesign.co.uk>
2017-09-28 10:16:51 +0100

    Remove other country election apps
    This is because we don't use them in the UK install/fork and we're
    unlikely to start doing so again in the future.
    
    Having the code here makes maintaining the UK code tricker especially
    when refactoring parts of the data model.

--- elections/ar_elections_2015/settings.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from __future__ import unicode_literals
-
-MAPIT_BASE_URL = 'http://argentina.mapit.staging.mysociety.org/'
-
-SITE_OWNER = 'YoQuieroSaber'
-COPYRIGHT_HOLDER = 'YoQuieroSaber'
--- elections/ar_elections_2015/views/__init__.py
+++
@@ -1,1 +0,0 @@
-from .frontpage import *
