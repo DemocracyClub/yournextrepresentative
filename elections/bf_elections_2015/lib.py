8170a368a20398f3421b78ac46eb7282c18aa377
Sym Roe <sym.roe@talusdesign.co.uk>
2017-09-28 10:16:51 +0100

    Remove other country election apps
    This is because we don't use them in the UK install/fork and we're
    unlikely to start doing so again in the future.
    
    Having the code here makes maintaining the UK code tricker especially
    when refactoring parts of the data model.

--- elections/bf_elections_2015/lib.py
+++
@@ -1,4 +0,0 @@
-from __future__ import unicode_literals
-
-def shorten_post_label(post_label):
-    return post_label
--- elections/bf_elections_2015/views/frontpage.py
+++
@@ -1,6 +0,0 @@
-from __future__ import unicode_literals
-
-from candidates.views import AddressFinderView
-
-class BurkinaFasoAddressFinder(AddressFinderView):
-    country = 'Burkina Faso'
