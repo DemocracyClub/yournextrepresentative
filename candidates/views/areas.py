3064398f33e6c6bb2516d32ccc3d7d2313e38447
Sym Roe <sym.roe@talusdesign.co.uk>
2018-01-05 16:55:20 +0000

    Remove the Areas view

--- candidates/views/areas.py
+++ candidates/views/areas.py
@@ -41,112 +41,12 @@ class AreasView(TemplateView):
     template_name = 'candidates/areas.html'
 
     def get(self, request, *args, **kwargs):
-        self.types_and_areas = []
-        for type_and_area in kwargs['type_and_area_ids'].split(','):
-            if '--' not in type_and_area:
-                message = _("Malformed type and area: '{0}'")
-                return HttpResponseBadRequest(message.format(type_and_area))
-            self.types_and_areas.append(type_and_area.split('--', 1))
-        response = super(AreasView, self).get(request, *args, **kwargs)
-        return response
-
-    def get_context_data(self, **kwargs):
-        context = super(AreasView, self).get_context_data(**kwargs)
-        all_area_names = set()
-        context['posts'] = []
-        any_area_found = False
-        no_data_areas = []
-        posts_seen = set()
-        for area_type_code, area_id in self.types_and_areas:
-            area_extras = AreaExtra.objects \
-                .select_related('base', 'type') \
-                .prefetch_related('base__posts') \
-                .filter(base__identifier=area_id)
-            if area_extras.exists():
-                any_area_found = True
-            else:
-                continue
-
-            for area_extra in area_extras:
-                area = area_extra.base
-                all_area_names.add(area.name)
-                for post in area.posts.all():
-                    post_extra = post.extra
-                    try:
-                        election = post_extra.elections.get(current=True)
-                    except Election.DoesNotExist:
-                        continue
-                    if post.id in posts_seen:
-                        continue
-                    else:
-                        posts_seen.add(post.id)
-                    locked = is_post_locked(post, election)
-                    extra_qs = MembershipExtra.objects.select_related('election')
-                    current_candidacies, _ = split_candidacies(
-                        election,
-                        post.memberships.prefetch_related(
-                            Prefetch('extra', queryset=extra_qs)
-                        ).select_related(
-                            'person', 'person__extra', 'on_behalf_of',
-                            'on_behalf_of__extra', 'organization'
-                        ).all()
-                    )
-                    elected_candidacies, unelected_candidacies = split_by_elected(
-                        election,
-                        current_candidacies,
-                    )
-                    elected_candidacies = group_candidates_by_party(
-                        election,
-                        elected_candidacies,
-                    )
-                    unelected_candidacies = group_candidates_by_party(
-                        election,
-                        unelected_candidacies,
-                    )
-                    post_context = {
-                        'election': election.slug,
-                        'election_data': election,
-                        'post_data': {
-                            'id': post.extra.slug,
-                            'label': post.label,
-                        },
-                        'candidates_locked': locked,
-                        'lock_form': ToggleLockForm(
-                            initial={
-                                'post_id': post.extra.slug,
-                                'lock': not locked
-                            },
-                        ),
-                        'candidate_list_edits_allowed':
-                        get_edits_allowed(self.request.user, locked),
-                        'has_elected': \
-                            len(elected_candidacies['parties_and_people']) > 0,
-                        'elected': elected_candidacies,
-                        'unelected': unelected_candidacies,
-                        'add_candidate_form': NewPersonForm(
-                            election=election.slug,
-                            initial={
-                                ('constituency_' + election.slug): post_extra.slug,
-                                ('standing_' + election.slug): 'standing',
-                            },
-                            hidden_post_widget=True,
-                        ),
-                    }
-
-                    post_context = get_person_form_fields(
-                        post_context,
-                        post_context['add_candidate_form']
-                    )
-
-                    context['posts'].append(post_context)
-
-        context['no_data_areas'] = no_data_areas
-
-        if not any_area_found:
-            raise Http404("No such areas found")
-        context['all_area_names'] = ' — '.join(all_area_names)
-        context['suppress_official_documents'] = True
-        return context
+        """
+        Set a HTTP 410: GONE status to indicate that this URL shouldn't be
+        indexed any more.
+        """
+        context = self.get_context_data(**kwargs)
+        return self.render_to_response(context, status=410)
 
 
 class PostsOfTypeView(TemplateView):
