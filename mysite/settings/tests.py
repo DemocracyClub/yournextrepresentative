4b9d0550702093b3dcd49be257de8874c2464bb5
Sym Roe <sym.roe@talusdesign.co.uk>
2018-01-17 19:04:20 +0000

    Refactor settings to be pure python
    This starts to bring the settings in to line with a) all the other DC
    projects and b) a pure python way of settings everything up

---
+++ mysite/settings/tests.py
@@ -0,0 +1,20 @@
+from .base import *  # noqa
+
+
+DATABASES['default']['CONN_MAX_AGE'] = 0
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
+    }
+}
+
+
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.MD5PasswordHasher',
+]
+
+RUNNING_TESTS = True
+
+
+SECRET_KEY = "just here for testing"
