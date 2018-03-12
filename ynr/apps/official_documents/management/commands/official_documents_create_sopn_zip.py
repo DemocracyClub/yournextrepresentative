df53cbe032dea9073b9447a76c560613066871b8
Sym Roe <sym.roe@talusdesign.co.uk>
2018-03-12 13:02:00 +0000

    Management command for putting all SOPNs in a zip file

---
+++ ynr/apps/official_documents/management/commands/official_documents_create_sopn_zip.py
@@ -0,0 +1,33 @@
+import os
+import zipfile
+
+from django.core.management.base import BaseCommand
+from django.conf import settings
+
+from official_documents.models import OfficialDocument
+
+
+class Command(BaseCommand):
+
+    help = "Create a ZIP file containing all SOPN PDFs"
+
+    def handle(self, *args, **options):
+        zip_file_path = os.path.join(
+            settings.MEDIA_ROOT,
+            "all_sopns.zip"
+        )
+        with zipfile.ZipFile(zip_file_path, 'w') as out_zip:
+            self.add_files_to_zip(out_zip)
+
+    def add_files_to_zip(self, out_zip):
+        for document in OfficialDocument.objects.all():
+            doc_name = os.path.join(
+                document.election.slug,
+                document.post.extra.slug,
+                str(document.pk)
+            )
+            doc_name = "{}.{}".format(
+                doc_name,
+                document.uploaded_file.path.split('.')[-1]
+            )
+            out_zip.write(document.uploaded_file.path, doc_name)
