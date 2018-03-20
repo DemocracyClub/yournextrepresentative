8f56799318db8e6fb84f68030d7429e5d9163ea1
Sym Roe <sym.roe@talusdesign.co.uk>
2018-03-20 11:15:41 +0000

    Management command for removing queued images without files
    The _cause_ of QueuedImages not having a file attached isn't clear, but
    the fact is that some of them are in a bad state where they cause a 500.
    
    This command lists and deltes them.
    
    Refs #308

---
+++ ynr/apps/moderation_queue/management/commands/moderation_queue_remove_queued_without_images.py
@@ -0,0 +1,31 @@
+from __future__ import unicode_literals
+
+from django.core.management.base import BaseCommand
+
+from moderation_queue.models import QueuedImage
+
+
+class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--delete',
+            action='store_true',
+            help=('Delete moderation queue items with no image')
+        )
+
+    def handle(self, **options):
+        qs = QueuedImage.objects.filter(decision='undecided')
+        for qi in qs:
+            try:
+                qi.image.file
+            except (ValueError, IOError):
+                if options['delete']:
+                    self.stdout.write(
+                        "Deleting {} because it has no image".format(
+                            qi
+                        ))
+                    qi.delete()
+                else:
+                    self.stdout.write("{} has no image".format(
+                        qi
+                    ))
