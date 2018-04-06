93c76c8c43f652c6a38add82fb64e6f001c2135e
Sym Roe <sym.roe@talusdesign.co.uk>
2018-04-06 12:08:04 +0100

    Management command for exporting SOPN sourcing CSV
    This is used for creating the CSV files that we normally  add to Google Sheets for sourcing SOPNs.
    
    The output of this should be importable by
    candidates_import_statements_of_persons_nominated

---
+++ ynr/apps/official_documents/management/commands/official_documents_create_sopn_sourcing_csv.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function, unicode_literals
+
+from compat import BufferDictWriter
+
+from django.core.management.base import BaseCommand
+
+from candidates.models import PostExtraElection
+
+
+class Command(BaseCommand):
+
+    help = """
+    Create a CSV file that can be populated and imported by
+    candidates_import_statements_of_persons_nominated
+    """
+
+    fieldnames = [
+        'ballot_paper_id',
+        'Election name',
+        'Area name',
+        'Council webpage likely to link to SOPN',
+        'Link to PDF',
+        'Notes',
+    ]
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--election-date',
+            action='store',
+            required=True
+        )
+
+    def handle(self, *args, **options):
+
+        out_csv = BufferDictWriter(self.fieldnames)
+        out_csv.writeheader()
+
+        qs = PostExtraElection.objects.filter(
+            election__election_date=options['election_date']
+        ).select_related(
+            'election', 'postextra', 'postextra__base'
+        ).order_by(
+            'election__slug'
+        )
+
+        for pee in qs:
+            row = {
+                'ballot_paper_id': pee.ballot_paper_id,
+                'Election name': pee.election.name,
+                'Area name': pee.postextra.base.label,
+                'Council webpage likely to link to SOPN': '',
+                'Link to PDF': '',
+                'Notes': '',
+
+            }
+            out_csv.writerow(row)
+
+        self.stdout.write(out_csv.output)
