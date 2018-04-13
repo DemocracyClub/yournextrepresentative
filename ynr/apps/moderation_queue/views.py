42b49d35041c90a63926644576830ebd727692df
Sym Roe <sym.roe@talusdesign.co.uk>
2018-04-13 22:50:15 +0100

    Better find PDF pages

--- ynr/apps/moderation_queue/views.py
+++ ynr/apps/moderation_queue/views.py
@@ -624,7 +624,7 @@ class SuggestLockReviewListView(LoginRequiredMixin, TemplateView):
         else:
             qs = qs.exclude(suggestedpostlock__user=self.request.user)
 
-        return qs[:100]
+        return qs[:10]
 
     def get_context_data(self, **kwargs):
         context = super(
