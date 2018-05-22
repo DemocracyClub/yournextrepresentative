89011e8c6c58fe2f26fad0c0ee64e48d4ddbf7c9
Sym Roe <sym.roe@talusdesign.co.uk>
2018-05-22 22:24:07 +0100

    Fix migration parent

--- ynr/apps/candidates/migrations/0044_remove_membership_fk_to_election.py
+++ ynr/apps/candidates/migrations/0044_remove_membership_fk_to_election.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('candidates', '0044_delete_membership_extra'),
+        ('candidates', '0043_remove_simple_popolo_field'),
     ]
 
     operations = [
