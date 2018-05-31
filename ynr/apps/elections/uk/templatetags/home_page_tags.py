8d35f736db3e864ca1416d8dd1169b355e61ec0b
Sym Roe <sym.roe@talusdesign.co.uk>
2018-05-31 11:42:10 +0100

    Move to a single FRONT_PAGE_CTA setting

--- ynr/apps/elections/uk/templatetags/home_page_tags.py
+++ ynr/apps/elections/uk/templatetags/home_page_tags.py
@@ -69,7 +69,7 @@ def election_night_countil_control_progress(context):
 def sopn_import_progress(context):
 
     context['SHOW_SOPN_TRACKER'] = getattr(
-        settings, 'SHOW_SOPN_TRACKER', False)
+        settings, 'FRONT_PAGE_CTA', False) == "SOPN_TRACKER"
     if context['SHOW_SOPN_TRACKER']:
         context['sopn_tracker_election_name'] \
             = settings.SOPN_TRACKER_INFO['election_name']
@@ -96,7 +96,7 @@ def person_tasks(context):
                         takes_context=True)
 def current_election_stats(context):
     context['SHOW_ELECTION_STATS'] = getattr(
-        settings, 'SHOW_ELECTION_STATS', False)
+        settings, 'FRONT_PAGE_CTA', False) == "ELECTION_STATS"
 
     if context['SHOW_ELECTION_STATS']:
         context['election_name'] \
@@ -122,7 +122,7 @@ def current_election_stats(context):
                         takes_context=True)
 def results_progress(context):
     context['SHOW_RESULTS_PROGRESS'] = getattr(
-        settings, 'SHOW_RESULTS_PROGRESS', False)
+        settings, 'FRONT_PAGE_CTA', False) == "RESULTS_PROGRESS"
 
     if context['SHOW_RESULTS_PROGRESS']:
         election_date = settings.SOPN_TRACKER_INFO['election_date']
@@ -147,7 +147,7 @@ def results_progress(context):
                         takes_context=True)
 def by_election_ctas(context):
     context['SHOW_BY_ELECTION_CTA'] = getattr(
-        settings, 'SHOW_BY_ELECTION_CTA', False)
+        settings, 'FRONT_PAGE_CTA', False) == "BY_ELECTIONS"
 
     if context['SHOW_BY_ELECTION_CTA']:
         all_pees = PostExtraElection.objects.filter(
