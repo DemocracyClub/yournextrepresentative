896fbe54151556df2ea87f4ed55e4396a0e057fb
Sym Roe <sym.roe@talusdesign.co.uk>
2018-08-30 11:01:57 +0100

    Pull get_election out in to method
    This means we don't need to rely on the ordering of the mixins in order
    for this to be useful.

--- ynr/apps/elections/mixins.py
+++ ynr/apps/elections/mixins.py
@@ -7,9 +7,13 @@ from .models import Election
 class ElectionMixin(object):
     """A mixin to add election data from the URL to the context"""
 
+    def get_election(self):
+        election = self.kwargs["election"]
+        return get_object_or_404(Election, slug=election)
+
     def dispatch(self, request, *args, **kwargs):
         self.election = election = self.kwargs["election"]
-        self.election_data = get_object_or_404(Election, slug=election)
+        self.election_data = self.get_election()
         return super().dispatch(request, *args, **kwargs)
 
     def get_context_data(self, **kwargs):
