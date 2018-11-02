361ca26c65b268dc117ac7dfc874af0505e564d7
Sym Roe <sym.roe@talusdesign.co.uk>
2018-11-02 13:43:55 +0000

    Add Enum for PersonIdentifier Fields
    This is similar to the work to remove SimplePopoloFields from the
    database layer. The values here are all the values we want in the
    initial version of the "n-links" on the person page model.
    
    One day we want to enable any type of link, specified by the user.
    That's too big a change to do all at once, so step one is to remove
    ComplexPopoloFields in the database but still use a fixed list of
    fields.

--- ynr/apps/ynr_refactoring/settings.py
+++ ynr/apps/ynr_refactoring/settings.py
@@ -1,3 +1,19 @@
+from enum import Enum, unique
+
 from .helpers.popolo_fields import simple_fields
 
 SIMPLE_POPOLO_FIELDS = simple_fields
+
+
+@unique
+class PersonIdentifierFields(Enum):
+    email = "Email"
+    facebook_page_url = "Facebook Page"
+    facebook_personal_url = "Facebook Personal"
+    homepage_url = "Homepage"
+    linkedin_url = "Linkedin"
+    party_ppc_page_url = "Party PPC Page"
+    twitter_username = "Twitter"
+    wikipedia_url = "Wikipedia"
+    # party_candidate_page = "Party Candidate Page"
+    # other = "Other"
