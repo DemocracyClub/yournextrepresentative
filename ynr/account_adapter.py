81fd01e5d605dd094f7983469a99cba81664b903
Sym Roe <sym.roe@talusdesign.co.uk>
2018-11-07 13:49:58 +0000

    Log social app authentication errors

--- ynr/account_adapter.py
+++ ynr/account_adapter.py
@@ -1,4 +1,9 @@
+import logging
+
 from allauth.account.adapter import DefaultAccountAdapter
+from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
+
+logger = logging.getLogger(__name__)
 
 
 class NoNewUsersAccountAdapter(DefaultAccountAdapter):
@@ -12,3 +17,30 @@ class NoNewUsersAccountAdapter(DefaultAccountAdapter):
         (Comment reproduced from the overridden method.)
         """
         return False
+
+
+class LoggingSocialAccountAdapter(DefaultSocialAccountAdapter):
+    """
+    Exactly the same as the DefaultSocialAccountAdapter, but logs authentication
+    errors to the default log.
+
+    """
+
+    def authentication_error(
+        self,
+        request,
+        provider_id,
+        error=None,
+        exception=None,
+        extra_context=None,
+    ):
+        """
+        Log errors to authenticating. This method in the parent class is
+        left blank and exists for overriding, so we can do what we want here.
+
+        """
+        logger.error(
+            "Error logging in with provider '{}' with error '{}' ({})".format(
+                provider_id, error, exception
+            )
+        )
