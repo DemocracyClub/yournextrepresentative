dbbe924b42525e827b4bdcd49ef9da3d41d41423
Sym Roe <sym.roe@talusdesign.co.uk>
2019-01-29 17:09:01 +0000

    Test that the dest person keeps set values, and gets unset values

--- ynr/apps/people/tests/test_merging.py
+++ ynr/apps/people/tests/test_merging.py
@@ -205,3 +205,19 @@ class TestMerging(TestUserMixin, UK2015ExamplesMixin, WebTest):
         )
         self.assertEqual(actual[0]["data"], expected_versions[0]["data"])
         self.assertEqual(len(actual), len(expected_versions))
+
+    def test_dest_person_keeps_properties(self):
+        self.dest_person.birth_date = "1956-01-02"
+        self.source_person.birth_date = "1945-01-04"
+
+        merger = PersonMerger(self.dest_person, self.source_person)
+        merger.merge()
+        self.assertEqual(self.dest_person.birth_date, "1956-01-02")
+
+    def test_dest_person_gets_empty_values_from_source(self):
+        self.dest_person.birth_date = None
+        self.source_person.birth_date = "1945-01-04"
+
+        merger = PersonMerger(self.dest_person, self.source_person)
+        merger.merge()
+        self.assertEqual(self.dest_person.birth_date, "1945-01-04")
