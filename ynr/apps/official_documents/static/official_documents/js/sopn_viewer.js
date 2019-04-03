6252550fe18ae0cd66e7ce521e7c1229208ce5c3
Sym Roe <sym.roe@talusdesign.co.uk>
2019-04-03 20:06:48 +0100

    Properly display single pages of multipage PDFs

--- ynr/apps/official_documents/static/official_documents/js/sopn_viewer.js
+++ ynr/apps/official_documents/static/official_documents/js/sopn_viewer.js
@@ -4,7 +4,7 @@ var SOPN_VIEWER = (function () {
     var module = {};
 
     function load_pages_by_range(pdf, start_page, end_page) {
-        if (start_page > pdf.numPages || end_page < pdf.numPages) {
+        if (start_page > pdf.numPages || start_page === end_page + 1) {
             return;
         }
         pdf.getPage(start_page).then(function (page) {
