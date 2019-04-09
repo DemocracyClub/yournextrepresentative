e139978891c675afdf380da50bb7dd6901e0f3c5
Sym Roe <sym.roe@talusdesign.co.uk>
2019-04-09 19:00:02 +0100

    Validate email and twitter fields when updating PersonIdentifiers

--- ynr/apps/candidates/tests/test_validators.py
+++ ynr/apps/candidates/tests/test_validators.py
@@ -16,46 +16,6 @@ class TestValidators(UK2015ExamplesMixin, TestCase):
         self.person = PersonFactory.create(name="John Doe")
 
     @skip("PersonIdentifiers are on Person Form")
-    def test_twitter_bad_url(self):
-        form = BasePersonForm(
-            {"name": "John Doe", "twitter_username": "http://example.org/blah"}
-        )
-        self.assertFalse(form.is_valid())
-        self.assertEqual(
-            form.errors,
-            {
-                "twitter_username": [
-                    "The Twitter username must only consist of alphanumeric characters or underscore"
-                ]
-            },
-        )
-
-    @skip("PersonIdentifiers are on Person Form")
-    def test_twitter_fine(self):
-        form = BasePersonForm(
-            {"name": "John Doe", "twitter_username": "madeuptwitteraccount"}
-        )
-        self.assertTrue(form.is_valid())
-        self.assertEqual(form.errors, {})
-        self.assertEqual(
-            form.cleaned_data["twitter_username"], "madeuptwitteraccount"
-        )
-
-    @skip("PersonIdentifiers are on Person Form")
-    def test_twitter_full_url(self):
-        form = BasePersonForm(
-            {
-                "name": "John Doe",
-                "twitter_username": "https://twitter.com/madeuptwitteraccount",
-            }
-        )
-        self.assertTrue(form.is_valid())
-        self.assertEqual(form.errors, {})
-        self.assertEqual(
-            form.cleaned_data["twitter_username"], "madeuptwitteraccount"
-        )
-
-    @skip("PersonIdentifiers are on Person Form")
     def test_malformed_email(self):
         form = BasePersonForm(
             {"name": "John Bercow", "email": "foo bar!"},
