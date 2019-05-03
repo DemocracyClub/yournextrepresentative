293cbd9ac1ad6c8f53e40fa36c3fdce6d9dda7ec
Sym Roe <sym.roe@talusdesign.co.uk>
2019-05-03 15:09:01 +0100

    Speed up results API view

--- ynr/apps/uk_results/views/api.py
+++ ynr/apps/uk_results/views/api.py
@@ -1,7 +1,9 @@
 from rest_framework import viewsets
 from django_filters import filters, filterset
+from django.db.models import Prefetch
 
 from api.v09.views import ResultsSetPagination
+from popolo.models import Membership
 
 from ..models import CandidateResult, ResultSet
 from ..serializers import CandidateResultSerializer, ResultSetSerializer
@@ -27,9 +29,23 @@ class ResultSetFilter(filterset.FilterSet):
 
 
 class ResultSetViewSet(viewsets.ModelViewSet):
-    queryset = ResultSet.objects.select_related(
-        "post_election__post", "user"
+    queryset = ResultSet.objects.prefetch_related(
+        "post_election__post",
+        "post_election__election",
+        "user",
+        Prefetch(
+            "candidate_results",
+            CandidateResult.objects.select_related(
+                "membership__party",
+                "membership__post",
+                "membership__person",
+                "membership__post_election",
+                "membership__post_election__post",
+                "membership__post_election__election",
+            ),
+        ),
     ).order_by("id")
+
     serializer_class = ResultSetSerializer
     pagination_class = ResultsSetPagination
 
