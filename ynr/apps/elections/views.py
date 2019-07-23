32f51c39d2be9b3b21932d31ee04540df10ebe58
Sym Roe <sym.roe@talusdesign.co.uk>
2019-07-23 10:52:24 +0100

    Add stripped down ballot view
    This is the very basics, with a new load of HTML and view code, for
    showing candidates for a ballot.
    
    A lot of features are missing in this commit, but it's too complex to
    replace them all in one go.
    
    The HTML might be best ignored as we're about to change the design of
    this page anyway. The view code might be best looked at as a new view,
    rather than looking at the diff.

--- ynr/apps/elections/views.py
+++ ynr/apps/elections/views.py
@@ -1,14 +1,11 @@
-from django.core.exceptions import ValidationError
 from django.db import transaction
 from django.db.models import Prefetch, Count
 from django.http import JsonResponse, HttpResponseRedirect, HttpResponse
 from django.shortcuts import get_object_or_404
 from django.utils.decorators import method_decorator
-from django.views import View
 from django.views.generic import UpdateView
 from django.views.decorators.cache import cache_control
 from django.views.generic import DetailView, TemplateView
-from slugify import slugify
 
 from auth_helpers.views import GroupRequiredMixin
 from candidates.csv_helpers import memberships_dicts_for_csv, list_to_csv
@@ -19,22 +16,14 @@ from candidates.models import (
     TRUSTED_TO_LOCK_GROUP_NAME,
     LoggedAction,
 )
-from candidates.models.auth import is_post_locked
 from candidates.views import get_client_ip
-from candidates.views.helpers import (
-    get_person_form_fields,
-    group_candidates_by_party,
-    split_by_elected,
-    split_candidacies,
-)
 from elections.mixins import ElectionMixin
 from elections.models import Election
-from moderation_queue.forms import SuggestedPostLockForm
-from moderation_queue.models import SuggestedPostLock
 from official_documents.models import OfficialDocument
-from people.forms import NewPersonForm, PersonIdentifierFormsetFactory
-from popolo.models import Membership, Post
+from popolo.models import Membership
 from parties.models import Party
+
+from .filters import BallotFilter, filter_shortcuts
 
 
 class ElectionView(DetailView):
@@ -138,7 +127,6 @@ class BallotPaperView(TemplateView):
         return super().dispatch(*args, **kwargs)
 
     def get_context_data(self, **kwargs):
-        from candidates.election_specific import shorten_post_label
 
         context = super().get_context_data(**kwargs)
 
@@ -147,133 +135,27 @@ class BallotPaperView(TemplateView):
             ballot_paper_id=context["election"],
         )
 
+        context["has_lock_suggestion"] = ballot.suggestedpostlock_set.exists()
+
+        context["candidates"] = Membership.objects.memberships_for_ballot(
+            ballot
+        )
+        context["candidate_list_edits_allowed"] = get_edits_allowed(
+            self.request.user, ballot.candidates_locked
+        )
+
         try:
             context["sopn"] = ballot.sopn
         except OfficialDocument.DoesNotExist:
             context["sopn"] = None
 
-        mp_post = context["ballot"].post
-        context["election"] = election = context["ballot"].election
-        context["post_id"] = post_id = mp_post.slug
-        context["post_obj"] = mp_post
-
-        context["post_label"] = mp_post.label
-        context["post_label_shorter"] = shorten_post_label(
-            context["post_label"]
-        )
-
-        context["redirect_after_login"] = context["ballot"].get_absolute_url()
-
-        context["post_data"] = {"id": mp_post.slug, "label": mp_post.label}
-
-        context["candidates_locked"] = ballot.candidates_locked
-
-        context["has_lock_suggestion"] = SuggestedPostLock.objects.filter(
-            ballot=ballot
-        ).exists()
-
-        if self.request.user.is_authenticated:
-            context[
-                "current_user_suggested_lock"
-            ] = SuggestedPostLock.objects.filter(
-                user=self.request.user, ballot=ballot
-            ).exists()
-
-        context["suggest_lock_form"] = SuggestedPostLockForm(
-            initial={"ballot": ballot}
-        )
-
-        if self.request.user.is_authenticated:
-            context[
-                "user_has_suggested_lock"
-            ] = SuggestedPostLock.objects.filter(
-                user=self.request.user, ballot=ballot
-            ).exists()
-
-        context["lock_form"] = ToggleLockForm(
-            initial={
-                "post_id": post_id,
-                "lock": not context["candidates_locked"],
-            }
-        )
-
-        context["candidate_list_edits_allowed"] = get_edits_allowed(
-            self.request.user, context["candidates_locked"]
-        )
-
-        extra_qs = Membership.objects.select_related("ballot__election")
-        current_candidacies, past_candidacies = split_candidacies(
-            election,
-            mp_post.memberships.select_related(
-                "person", "party", "ballot__election"
-            ).all(),
-        )
-
-        current_candidates = {c.person for c in current_candidacies}
-        past_candidates = {c.person for c in past_candidacies}
-
-        other_candidates = past_candidates - current_candidates
-
-        elected, unelected = split_by_elected(election, current_candidacies)
-
-        # Now split those candidates into those that we know aren't
-        # standing again, and those that we just don't know about.
-        not_standing_candidates = {
-            p for p in other_candidates if election in p.not_standing.all()
-        }
-        might_stand_candidates = {
-            p for p in other_candidates if election not in p.not_standing.all()
-        }
-        might_stand_candidates = {
-            p for p in might_stand_candidates if p.death_date == ""
-        }
-
-        not_standing_candidacies = [
-            c for c in past_candidacies if c.person in not_standing_candidates
-        ]
-        might_stand_candidacies = [
-            c for c in past_candidacies if c.person in might_stand_candidates
-        ]
-
-        context["candidacies_not_standing_again"] = group_candidates_by_party(
-            election, not_standing_candidacies
-        )
-
-        context["candidacies_might_stand_again"] = group_candidates_by_party(
-            election, might_stand_candidacies
-        )
-
-        context["elected"] = group_candidates_by_party(
-            election, elected, show_all=True
-        )
-        context["unelected"] = group_candidates_by_party(election, unelected)
-
-        context["show_retract_result"] = False
-        number_of_winners = 0
-        for c in current_candidacies:
-            if c.elected:
-                number_of_winners += 1
-            if c.elected is not None:
-                context["show_retract_result"] = True
-
-        max_winners = get_max_winners(ballot)
-        context["show_confirm_result"] = bool(max_winners)
-        if not ballot.candidates_locked:
-            context["add_candidate_form"] = NewPersonForm(
-                election=election.slug,
-                initial={
-                    ("constituency_" + election.slug): post_id,
-                    ("standing_" + election.slug): "standing",
-                },
-                hidden_post_widget=True,
-            )
-
-            context = get_person_form_fields(
-                context, context["add_candidate_form"]
-            )
-            context["identifiers_formset"] = PersonIdentifierFormsetFactory()
-
         return context
+
+        # TODO: SuggestedPostLocks
+        # TODO: Lock form
+        # TODO: past_candidates / standing again / etc
+        # TODO: Retract results
+        # TODO: New candidate form
 
 
 class LockBallotView(GroupRequiredMixin, UpdateView):
