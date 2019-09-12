55b68f804a99c1a51ae14cdf30e64e06e79ad7b0
Sym Roe <sym.roe@talusdesign.co.uk>
2019-09-12 11:32:04 +0100

    Create a LoggedAction for changing edit limitation status

--- ynr/apps/people/admin.py
+++ ynr/apps/people/admin.py
@@ -3,8 +3,10 @@ from django import forms
 
 from sorl.thumbnail.admin.current import AdminImageWidget
 
+from candidates.models import LoggedAction
+from candidates.views.version_data import get_client_ip
 from popolo.models import Membership
-from people.models import Person, PersonImage
+from people.models import Person, PersonImage, EditLimitationStatuses
 
 
 class MembershipInline(admin.StackedInline):
@@ -62,6 +64,27 @@ class PersonAdmin(admin.ModelAdmin):
 
     list_filter = ("edit_limitations",)
     inlines = [PersonImageInline]
+
+    def save_model(self, request, obj, form, change):
+        if form.initial["edit_limitations"] != form["edit_limitations"].value():
+
+            try:
+                limitation = EditLimitationStatuses[
+                    form["edit_limitations"].value()
+                ].value
+                message = "Changed edit limitations to '{}'".format(limitation)
+            except KeyError:
+                message = "Removed edit limitations"
+
+            LoggedAction.objects.create(
+                user=request.user,
+                action_type="change-edit-limitations",
+                ip_address=get_client_ip(request),
+                person=obj,
+                source=message,
+            )
+
+        super().save_model(request, obj, form, change)
 
 
 admin.site.register(Person, PersonAdmin)
