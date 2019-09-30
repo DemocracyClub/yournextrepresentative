e34b96a45d2c4360840740c1214483c3581e5de9
Sym Roe <sym.roe@talusdesign.co.uk>
2019-09-30 20:59:32 +0100

    Don't try to delete some Memberships when reverting
    Memberships for locked ballots and memberships with results shouldn't be
    reverted.
    
    This is a little confusing as it stands, see #1035 for more

--- ynr/apps/candidates/tests/test_changes_for_review.py
+++ ynr/apps/candidates/tests/test_changes_for_review.py
@@ -279,7 +279,6 @@ class TestNeedsReviewFeed(UK2015ExamplesMixin, TestUserMixin, WebTest):
         form["constituency_parl.2015-05-07"] = "65808"
         form["party_GB_parl.2015-05-07"] = str(Party.objects.first().ec_id)
         response = form.submit().follow()
-
         person = response.context["person"]
         self.person = person
 
