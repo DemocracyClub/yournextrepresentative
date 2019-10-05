7359a35297fae208b679c773ada06a3c049252c6
Sym Roe <sym.roe@talusdesign.co.uk>
2019-10-05 18:19:42 +0100

    Slim down Elections end point

--- ynr/apps/elections/serializers.py
+++ ynr/apps/elections/serializers.py
@@ -26,21 +26,13 @@ class ElectionSerializer(MinimalElectionSerializer):
     class Meta:
         model = election_models.Election
         fields = (
-            "id",
+            "slug",
             "url",
             "name",
-            "for_post_role",
-            "winner_membership_role",
-            "candidate_membership_role",
             "election_date",
             "current",
-            "use_for_candidate_suggestions",
             "organization",
             "party_lists_in_use",
-            "default_party_list_members_to_show",
-            "show_official_documents",
-            "ocd_division",
-            "description",
         )
 
     organization = OrganizationSerializer(read_only=True)
