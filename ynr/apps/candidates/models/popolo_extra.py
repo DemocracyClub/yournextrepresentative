6df2cc9f874960aadb10a7969942fedf14af93cd
Sym Roe <sym.roe@talusdesign.co.uk>
2020-04-02 19:02:30 +0100

    Disable select options for locked and cancelled elections
    This commit is doing a lot:
    
    1. DRY up the code for getting a list of ballots in to a select widget
    for a given election
    
    2. Change the widget to a custom one that can disable options
    
    3. Disable ballots that are cancelled or locked, and show a message
    explaining this

--- ynr/apps/candidates/models/popolo_extra.py
+++ ynr/apps/candidates/models/popolo_extra.py
@@ -254,14 +254,20 @@ class Ballot(models.Model):
         if not user.is_authenticated:
             return False
 
-        # If the ballot is unlocked, anyone can edit the memberships
-        if not self.candidates_locked:
+        # If the ballot is unlocked and not cancelled, anyone
+        # can edit the memberships
+        if not self.candidates_locked and not self.cancelled:
             return True
 
         if (
             allow_if_trusted_to_lock
             and user.groups.filter(name=TRUSTED_TO_LOCK_GROUP_NAME).exists()
         ):
+            return True
+
+        # Special case where elections are cancelled before they are locked
+        # Don't allow most people to edit them, but do allow staff to
+        if self.cancelled and not self.candidates_locked and user.is_staff:
             return True
 
         return False
