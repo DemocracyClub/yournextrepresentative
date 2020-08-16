bee6f64f19f5c910f38cae458274737e61dfd5a3
Sym Roe <sym.roe@talusdesign.co.uk>
2020-08-16 12:22:46 +0100

    Make sure we can't create duplicate duplicates

--- ynr/apps/duplicates/models.py
+++ ynr/apps/duplicates/models.py
@@ -10,6 +10,10 @@ from model_utils.models import StatusModel
 from model_utils import Choices
 
 
+def sort_people(person_a, person_b):
+    return sorted([person_a, person_b], key=lambda m: m.pk)
+
+
 class DuplicateSuggestionQuerySet(models.QuerySet):
     def for_person(self, person):
         return self.for_person_id(person.pk)
@@ -20,14 +24,26 @@ class DuplicateSuggestionQuerySet(models.QuerySet):
         )
 
     def for_both_people(self, person, other_person):
-        qs = self.filter(Q(person=person) | Q(person=other_person))
-        qs = qs.filter(Q(other_person=other_person) | Q(other_person=person))
+        person, other_person = sort_people(person, other_person)
+        qs = self.filter(person=person, other_person=other_person)
         return qs
 
     def marked_as_not_duplicate(self, person, other_person):
         qs = self.filter(status=self.model.STATUS.not_duplicate)
         qs = qs.for_both_people(person, other_person)
         return qs.exists()
+
+    def get_or_create(self, defaults=None, **kwargs):
+        kwargs["person"], kwargs["other_person"] = sort_people(
+            kwargs["person"], kwargs["other_person"]
+        )
+        return super().get_or_create(defaults, **kwargs)
+
+    def update_or_create(self, defaults=None, **kwargs):
+        kwargs["person"], kwargs["other_person"] = sort_people(
+            kwargs["person"], kwargs["other_person"]
+        )
+        return super().update_or_create(defaults, **kwargs)
 
 
 class DuplicateSuggestion(StatusModel, TimeStampedModel):
@@ -63,3 +79,16 @@ class DuplicateSuggestion(StatusModel, TimeStampedModel):
 
     class Meta:
         unique_together = ("person", "other_person")
+
+    def save(self, **kwargs):
+        """
+        Force the order of the IDs, making self.person always
+        the lower of the two IDs
+        """
+
+        self.person, self.other_person = sort_people(
+            self.person, self.other_person
+        )
+        assert self.person.pk < self.other_person.pk
+
+        return super().save(**kwargs)
