2514559f3eef95c55c43fd05c0dceebd9b116026
Sym Roe <sym.roe@talusdesign.co.uk>
2021-01-11 18:53:41 +0000

    Use JSONField in API rather than custom JSON serializer field

--- ynr/apps/api/helpers.py
+++ ynr/apps/api/helpers.py
@@ -1,15 +1,7 @@
-import json
-
-from rest_framework import pagination, serializers
+from rest_framework import pagination
 
 
 class DefaultPageNumberPagination(pagination.PageNumberPagination):
     page_size = 10
     page_size_query_param = "page_size"
     max_page_size = 200
-
-
-class JSONSerializerField(serializers.Field):
-    def to_representation(self, value):
-        if value:
-            return json.loads(value)
