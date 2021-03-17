eff0a17458ea4b7e7d8ef1c0e6708b6ca06f1951
Sym Roe <sym.roe@talusdesign.co.uk>
2021-03-17 18:39:55 +0000

    Move person URLs to people app

--- ynr/apps/candidates/urls.py
+++ ynr/apps/candidates/urls.py
@@ -1,6 +1,5 @@
 from django.conf import settings
 from django.conf.urls import include, url
-from django.views.decorators.cache import cache_page
 from django.views.generic import TemplateView
 from django.views.generic.base import RedirectView
 
@@ -103,61 +102,6 @@ urlpatterns = [
         r"^posts-of-type/(?P<post_type>.*?)(?:/(?P<ignored_slug>.*))?$",
         views.PostsOfTypeView.as_view(),
         name="posts-of-type-view",
-    ),
-    url(
-        r"^person/(?P<person_id>\d+)/revert$",
-        views.RevertPersonView.as_view(),
-        name="person-revert",
-    ),
-    url(
-        r"^person/(?P<person_id>\d+)/merge_conflict/(?P<other_person_id>\d+)/not_standing/$",
-        views.CorrectNotStandingMergeView.as_view(),
-        name="person-merge-correct-not-standing",
-    ),
-    url(
-        r"^person/(?P<person_id>\d+)/merge$",
-        views.MergePeopleView.as_view(),
-        name="person-merge",
-    ),
-    url(
-        r"^person/(?P<person_id>\d+)/other-names$",
-        views.PersonOtherNamesView.as_view(),
-        name="person-other-names",
-    ),
-    url(
-        r"^person/(?P<person_id>\d+)/other-names/create$",
-        views.PersonOtherNameCreateView.as_view(),
-        name="person-other-name-create",
-    ),
-    url(
-        r"^person/(?P<person_id>\d+)/other-name/(?P<pk>\d+)/delete$",
-        views.PersonOtherNameDeleteView.as_view(),
-        name="person-other-name-delete",
-    ),
-    url(
-        r"^person/(?P<person_id>\d+)/other-name/(?P<pk>\d+)/update/?$",
-        views.PersonOtherNameUpdateView.as_view(),
-        name="person-other-name-update",
-    ),
-    url(
-        r"^person/(?P<person_id>\d+)(?:/(?P<ignored_slug>.*))?$",
-        views.PersonView.as_view(),
-        name="person-view",
-    ),
-    url(
-        r"^person/create/select_election$",
-        views.NewPersonSelectElectionView.as_view(),
-        name="person-create-select-election",
-    ),
-    url(
-        r"^person/(?P<person_id>\d+)/update/?$",
-        views.UpdatePersonView.as_view(),
-        name="person-update",
-    ),
-    url(
-        r"^person/(?P<person_id>\d+)/update/single_election_form/(?P<election>[^/]+)$",
-        cache_page(60 * 60)(views.SingleElectionFormView.as_view()),
-        name="person-update-single-election",
     ),
 ]
 
