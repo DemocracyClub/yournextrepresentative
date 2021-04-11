9c724de9fd14f54389f6cad7a86e380dd1475985
Sym Roe <sym.roe@talusdesign.co.uk>
2021-04-11 12:20:02 +0100

    Remove non-alphanumeric chars from query

--- ynr/apps/search/utils.py
+++ ynr/apps/search/utils.py
@@ -1,3 +1,5 @@
+import re
+
 from django.contrib.postgres.search import SearchQuery, SearchRank
 from django.db.models import Count, F
 
@@ -5,6 +7,8 @@ from people.models import Person
 
 
 def search_person_by_name(name, synonym=True):
+    name = name.lower()
+    name = re.sub(r"[^a-z ]", "", name)
     name = " ".join(name.strip().split())
     and_name = " & ".join(name.split(" "))
     or_name = " | ".join(name.split(" "))
