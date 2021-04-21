40c51e96bb752392ced3603cfc40ccfcd725d2e3
Michael Collins <michaeljcollinsuk@users.noreply.github.com>
2021-04-21 17:19:12 +0100

    Add integration test to check dup obj created

--- ynr/apps/candidates/tests/test_duplicate_suggestion_view.py
+++ ynr/apps/candidates/tests/test_duplicate_suggestion_view.py
@@ -6,6 +6,7 @@ import pytest
 from candidates.tests.auth import TestUserMixin
 
 from candidates.views.people import DuplicatePersonView
+from duplicates.models import DuplicateSuggestion
 from people.models import Person
 from people.tests.factories import PersonFactory
 
@@ -62,7 +63,7 @@ class TestDuplicatePersonViewUnitTests(TestUserMixin, TestCase):
         assert context["user_can_merge"] is True
 
     def test_get_context_data_other_person_added_to_context_user_cant_merge(
-        self
+        self,
     ):
         person = PersonFactory(pk=1234)
         self.view.request.user = self.user
@@ -71,3 +72,59 @@ class TestDuplicatePersonViewUnitTests(TestUserMixin, TestCase):
         assert "error" not in context
         assert context["other_person"] == person
         assert context["user_can_merge"] is False
+
+
+class TestDuplicatePersonViewIntegrationTests(TestUserMixin, WebTest, TestCase):
+    def setUp(self):
+        super().setUp()
+        self.person = PersonFactory()
+        self.other_person = PersonFactory()
+
+    def test_duplicate_suggestion_created(self):
+        assert DuplicateSuggestion.objects.count() == 0
+
+        url = reverse(
+            viewname="duplicate-person", kwargs={"person_id": self.person.pk}
+        )
+        url = f"{url}?other={self.other_person.pk}"
+        response = self.app.get(url, user=self.user)
+
+        # standard user so merge form should not be displayed
+        assert DuplicatePersonView.MERGE_FORM_ID not in response.forms
+        assert DuplicatePersonView.SUGGESTION_FORM_ID in response.forms
+
+        # submit the suggestion form and expect object to be created
+        suggestion_form = response.forms[DuplicatePersonView.SUGGESTION_FORM_ID]
+        response = suggestion_form.submit()
+
+        suggestion = DuplicateSuggestion.objects.filter(
+            person=self.person, other_person=self.other_person
+        ).first()
+        assert DuplicateSuggestion.objects.count() == 1
+        assert bool(suggestion) is True
+
+    def test_duplicate_suggestion_not_created(self):
+        # created a suggestion from another user
+        created_suggestion = DuplicateSuggestion.objects.create(
+            person=self.person,
+            other_person=self.other_person,
+            user=self.user_who_can_merge,
+        )
+        assert DuplicateSuggestion.objects.count() == 1
+
+        url = reverse(
+            viewname="duplicate-person", kwargs={"person_id": self.person.pk}
+        )
+        url = f"{url}?other={self.other_person.pk}"
+        response = self.app.get(url, user=self.user)
+
+        # submit the suggestion form and expect object to be created
+        suggestion_form = response.forms[DuplicatePersonView.SUGGESTION_FORM_ID]
+        response = suggestion_form.submit()
+
+        # get suggestion - pk should match with one we created
+        suggestion = DuplicateSuggestion.objects.filter(
+            person=self.person, other_person=self.other_person
+        ).first()
+        assert DuplicateSuggestion.objects.count() == 1
+        assert created_suggestion.pk == suggestion.pk
