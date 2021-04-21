fc9c41fba328093b3ab8755063760b9750e2a682
Michael Collins <michaeljcollinsuk@users.noreply.github.com>
2021-04-21 16:01:20 +0100

    Update view to create suggestion or merge
    - Add post method to create a DuplicateSuggestion
    - Remove trusted to merge permission requirement, instead check for
    permission to merge when settings context to determine which forms
    should be displayed to the user
    - Split template to use two forms, with one to make request to
    MergePeopleView
    - Refactor tests

--- ynr/apps/candidates/tests/test_duplicate_suggestion_view.py
+++ ynr/apps/candidates/tests/test_duplicate_suggestion_view.py
@@ -1,56 +1,73 @@
+from django.test.client import RequestFactory
+from django.test.testcases import TestCase
+from django.urls.base import reverse
+from django_webtest import WebTest
 import pytest
+from candidates.tests.auth import TestUserMixin
 
 from candidates.views.people import DuplicatePersonView
 from people.models import Person
 from people.tests.factories import PersonFactory
 
 
-class TestDuplicatePersonViewUnitTests:
-    @pytest.fixture
-    def view_obj(self, rf):
+class TestDuplicatePersonViewUnitTests(TestUserMixin, TestCase):
+    def setUp(self):
+        super().setUp()
+        rf = RequestFactory()
         request = rf.get("/")
         request.GET = {}
+        request.user = self.user_who_can_merge
         view = DuplicatePersonView()
         view.setup(request=request)
         view.object = Person(pk=1)
-        return view
+        self.view = view
 
-    def test_get_context_data_missing_other_param(self, view_obj):
-        context = view_obj.get_context_data()
+    def test_get_context_data_missing_other_param(self):
+        context = self.view.get_context_data()
         assert "error" in context
         assert context["error"] == "Other person ID is missing"
 
-    def test_get_context_data_missing_other_value(self, view_obj):
-        view_obj.request.GET.update({"other": None})
-        context = view_obj.get_context_data()
+    def test_get_context_data_missing_other_value(self):
+        self.view.request.GET.update({"other": None})
+        context = self.view.get_context_data()
         assert "error" in context
         assert context["error"] == "Other person ID is missing"
 
-    def test_get_context_data_other_not_number(self, view_obj):
-        view_obj.request.GET.update({"other": "foo"})
-        context = view_obj.get_context_data()
+    def test_get_context_data_other_not_number(self):
+        self.view.request.GET.update({"other": "foo"})
+        context = self.view.get_context_data()
         assert "error" in context
         assert context["error"] == "Malformed person ID foo"
 
-    def test_get_context_data_other_id_same_as_person(self, view_obj):
-        view_obj.request.GET.update({"other": "1"})
-        context = view_obj.get_context_data()
+    def test_get_context_data_other_id_same_as_person(self):
+        self.view.request.GET.update({"other": "1"})
+        context = self.view.get_context_data()
         assert "error" in context
         assert (
             context["error"] == "You can't merge a person (1) with themself (1)"
         )
 
-    @pytest.mark.django_db
-    def test_get_context_data_other_person_not_found(self, view_obj):
-        view_obj.request.GET.update({"other": "2"})
-        context = view_obj.get_context_data()
+    def test_get_context_data_other_person_not_found(self):
+        self.view.request.GET.update({"other": "2"})
+        context = self.view.get_context_data()
         assert "error" in context
         assert context["error"] == "Person not found with ID 2"
 
-    @pytest.mark.django_db
-    def test_get_context_data_other_person_added_to_context(self, view_obj):
+    def test_get_context_data_other_person_added_to_context(self):
         person = PersonFactory(pk=1234)
-        view_obj.request.GET.update({"other": "1234"})
-        context = view_obj.get_context_data()
+        self.view.request.GET.update({"other": "1234"})
+        context = self.view.get_context_data()
         assert "error" not in context
         assert context["other_person"] == person
+        assert context["user_can_merge"] is True
+
+    def test_get_context_data_other_person_added_to_context_user_cant_merge(
+        self
+    ):
+        person = PersonFactory(pk=1234)
+        self.view.request.user = self.user
+        self.view.request.GET.update({"other": "1234"})
+        context = self.view.get_context_data()
+        assert "error" not in context
+        assert context["other_person"] == person
+        assert context["user_can_merge"] is False
