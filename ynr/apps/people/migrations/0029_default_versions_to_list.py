855412ae2c2373ea170c2a26fa29de795625883e
Sym Roe <sym.roe@talusdesign.co.uk>
2021-04-25 12:47:32 +0100

    Rename duplicate migration

--- ynr/apps/people/migrations/0029_default_versions_to_list.py
+++ ynr/apps/people/migrations/0029_default_versions_to_list.py
@@ -6,7 +6,7 @@ from django.db import migrations
 
 class Migration(migrations.Migration):
 
-    dependencies = [("people", "0027_auto_20210401_0811")]
+    dependencies = [("people", "0028_add_default_to_extra_data")]
 
     operations = [
         migrations.AlterField(
