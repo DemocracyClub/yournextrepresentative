2cbbea2cab659139075b48a8e19c682ad27be3b4
Virginia Dooley <vcdooley@gmail.com>
2021-05-03 12:22:44 +0100

    JS refactor

--- ynr/apps/uk_results/static/js/results.js
+++ ynr/apps/uk_results/static/js/results.js
@@ -1,35 +1,43 @@
+$(function() {
+    console.log("I'm working")
+    // runs when the page loads
+    // debugger 
+    tiedWinners()    // runs when the form changes
+    $('#ballot_paper_results_form').on("change", tiedWinners)
+   
+    if($(".errorlist").length != 0){
+        alert("error!") // code that should be run if the validation error exists goes here 
+    }
+})
 
-$(document).ready(function() {
-    $('#ballot_paper_results_form').on('change', function() {
- 
-        var inputs = document.querySelectorAll('*[id*=id_memberships]')
-        var list = Object.values(inputs)
-        var inputArray = list.map(input => input.value)
-        
-        function NoneEmpty(inputArray) {
-            if (inputArray.includes(""))
-                return false
-            else return true
-        }
-        
-        var noEmptyValues = NoneEmpty(inputArray)
-        
-        if (noEmptyValues) {
-            for (var i = 0; i < inputArray.length; ++i) {
-                var max = 0;
-                max = Math.max(...inputArray);
-                var maxIndex = list.indexOf(max)
-                
-                if (maxIndex != inputs[i] && max == inputs[i].value) { 
-                    if (inputs[i].value == max) {
-                        inputs[i].nextElementSibling.style.display = "inline"
-                    } else {
-                        inputs[i].nextElementSibling.style.display = "none" 
-                    } 
+function tiedWinners() {
+    var inputs = document.querySelectorAll('*[id*=id_memberships]')
+    var list = Object.values(inputs)
+    var inputArray = list.map(input => input.value)
+    console.log("I'm working, too") 
+    function NoneEmpty(inputArray) {
+        if (inputArray.includes(""))
+            return false
+        else return true
+    }
+    
+    var noEmptyValues = NoneEmpty(inputArray)
+    
+    if (noEmptyValues) {
+        for (var i = 0; i < inputArray.length; ++i) {
+            var max = 0;
+            max = Math.max(...inputArray);
+            var maxIndex = list.indexOf(max)
+            
+            if (maxIndex != inputs[i] && max == inputs[i].value) { 
+                if (inputs[i].value == max) {
+                    inputs[i].nextElementSibling.style.display = "inline"
                 } else {
-                    return
-                }
+                    inputs[i].nextElementSibling.style.display = "none" 
+                } 
+            } else {
+                return
             }
         }
-    })
-})
+    }
+}
