a03813ed4adf620e935a3201e50b002a69d2115e
Michael Collins <michaeljcollinsuk@users.noreply.github.com>
2021-11-12 14:51:00 +0000

    Update js to show coin toss based on winner count
    Show the coin toss field based on the ballots winner count. E.g. if
    two candidates have the same second highest total of votes and the
    winner count is 2, show the coin toss checkbox for both these
    candidates but not the candidate with the highest votes.

--- ynr/apps/uk_results/static/js/results.js
+++ ynr/apps/uk_results/static/js/results.js
@@ -15,30 +15,55 @@ $(function() {
 function tiedWinners() {
     var inputs = document.querySelectorAll('*[id*=id_memberships]')
     var list = Object.values(inputs)
-    var inputArray = list.map(input => input.value)
+    var ballotWinnerCount = form.dataset.winnerCount
     
-    function NoneEmpty(inputArray) {
-        if (inputArray.includes(""))
+    // array of integers representing the vote count for each candidate,
+    // sorted lowest to highest
+    var voteCounts = list.map(input => parseInt(input.value)).sort(
+        function(a, b) {
+            return a - b;
+        }
+    );
+    // get an array of the winning vote counts e.g. if the ballot has a winner
+    // count of 2, the last 2 vote counts from the sorted array of vote counts
+    var winningVoteCounts = voteCounts.slice(-ballotWinnerCount)
+
+    function NoneEmpty(voteCounts) {
+        if (voteCounts.includes(""))
             return false
         else return true
     }
     
-    var noEmptyValues = NoneEmpty(inputArray)
+    var noEmptyValues = NoneEmpty(voteCounts)
     if (noEmptyValues) {
-        var max = Math.max(...inputArray);
+        // loop through all the candidates to build a array of winners
         var winners = []
-        for (var i = 0; i < inputArray.length; ++i) {
-            if (inputs[i].value == max) { 
-                winners.push(inputs[i])
+        for (var i = 0; i < voteCounts.length; ++i) {
+            var candidateVoteCount = parseInt(inputs[i].value)
+            if (winningVoteCounts.includes(candidateVoteCount)) {
+                winners.push(inputs[i]);
             };
         }
-        if (winners.length == 1 ) {
-            for (var i = 0; i < inputArray.length; ++i) {
+
+        // if the number of winners is the same as the ballots winner count,
+        // no need for any coin toss tiebreakers
+        if (winners.length == ballotWinnerCount ) {
+            for (var i = 0; i < voteCounts.length; ++i) {
                 inputs[i].nextElementSibling.style.display = "none" 
                 inputs[i].nextElementSibling.querySelector("*[id*=id_tied_vote_memberships]").checked = false
             }
+        // otherwise we have some winners with tied vote counts
         } else {
+            // find the lowest winning vote count
+            var lowestWinningVoteCount = winningVoteCounts[0]
             for (var i = 0; i < winners.length; ++i) {
+
+                // if the vote count of this candidate is higher than the lowest
+                // then they dont need a coin toss, skip them
+                if (parseInt(winners[i].value) > lowestWinningVoteCount) {
+                    continue;
+                }
+                // otherwise show the coin toss field for this candidate
                 winners[i].nextElementSibling.style.display = "inline" 
              }
         }
