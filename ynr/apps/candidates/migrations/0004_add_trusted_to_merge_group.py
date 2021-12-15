a4e152f981e45ea2796956bcd1900fbd7b9f5765
Michael Collins <michaeljcollinsuk@users.noreply.github.com>
2021-12-15 12:40:35 +0000

    Broken attempt to squash candidates migrations
    - Causes CircularDependencyError when running migrate

--- ynr/apps/candidates/migrations/0004_add_trusted_to_merge_group.py
+++ ynr/apps/candidates/migrations/0004_add_trusted_to_merge_group.py
@@ -1,10 +1,39 @@
 from django.db import migrations
+from django.contrib.auth.management import create_permissions
 
-from auth_helpers.migrations import (
-    get_migration_group_create,
-    get_migration_group_delete,
-)
 from candidates.models import TRUSTED_TO_MERGE_GROUP_NAME
+
+
+def add_trusted_to_merge_group(apps, schema_editor, create_if_missing=True):
+    group_name, permission_codenames = TRUSTED_TO_MERGE_GROUP_NAME, []
+    Permission = apps.get_model("auth", "Permission")
+    Group = apps.get_model("auth", "Group")
+    try:
+        permissions = Permission.objects.filter(
+            codename__in=permission_codenames
+        )
+    except Permission.DoesNotExist:
+        if create_if_missing:
+            # This is a way of making sure the permissions exist taken from:
+            # https://code.djangoproject.com/ticket/23422#comment:6
+            assert not getattr(apps, "models_module", None)
+            apps.models_module = True
+            create_permissions(apps, verbosity=0)
+            apps.models_module = None
+            return add_trusted_to_merge_group(
+                apps, schema_editor, create_if_missing=False
+            )
+        else:
+            raise
+    new_group = Group.objects.create(name=group_name)
+    for permission in permissions:
+        new_group.permissions.add(permission)
+
+
+def remove_trusted_to_merge_group(apps, schema_editor):
+
+    Group = apps.get_model("auth", "Group")
+    Group.objects.get(name=TRUSTED_TO_MERGE_GROUP_NAME).delete()
 
 
 class Migration(migrations.Migration):
@@ -13,7 +42,6 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.RunPython(
-            get_migration_group_create(TRUSTED_TO_MERGE_GROUP_NAME, []),
-            get_migration_group_delete(TRUSTED_TO_MERGE_GROUP_NAME),
+            add_trusted_to_merge_group, remove_trusted_to_merge_group
         )
     ]
--- ynr/apps/candidates/migrations/0006_auto_add_trusted_to_rename_group.py
+++ ynr/apps/candidates/migrations/0006_auto_add_trusted_to_rename_group.py
@@ -1,11 +1,40 @@
 from django.db import migrations
 
-from auth_helpers.migrations import (
-    get_migration_group_create,
-    get_migration_group_delete,
-)
+from django.contrib.auth.management import create_permissions
 
 TRUSTED_TO_RENAME_GROUP_NAME = "Trusted To Rename"
+
+
+def add_trusted_to_rename_group(apps, schema_editor, create_if_missing=True):
+    group_name, permission_codenames = TRUSTED_TO_RENAME_GROUP_NAME, []
+    Permission = apps.get_model("auth", "Permission")
+    Group = apps.get_model("auth", "Group")
+    try:
+        permissions = Permission.objects.filter(
+            codename__in=permission_codenames
+        )
+    except Permission.DoesNotExist:
+        if create_if_missing:
+            # This is a way of making sure the permissions exist taken from:
+            # https://code.djangoproject.com/ticket/23422#comment:6
+            assert not getattr(apps, "models_module", None)
+            apps.models_module = True
+            create_permissions(apps, verbosity=0)
+            apps.models_module = None
+            return add_trusted_to_rename_group(
+                apps, schema_editor, create_if_missing=False
+            )
+        else:
+            raise
+    new_group = Group.objects.create(name=group_name)
+    for permission in permissions:
+        new_group.permissions.add(permission)
+
+
+def remove_trusted_to_rename_group(apps, schema_editor):
+
+    Group = apps.get_model("auth", "Group")
+    Group.objects.get(name=TRUSTED_TO_RENAME_GROUP_NAME).delete()
 
 
 class Migration(migrations.Migration):
@@ -14,7 +43,6 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.RunPython(
-            get_migration_group_create(TRUSTED_TO_RENAME_GROUP_NAME, []),
-            get_migration_group_delete(TRUSTED_TO_RENAME_GROUP_NAME),
+            add_trusted_to_rename_group, remove_trusted_to_rename_group
         )
     ]
