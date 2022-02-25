a593b1333de71a2e2765a782d6c3110b5570de77
Michael Collins <michaeljcollinsuk@users.noreply.github.com>
2022-02-25 16:24:14 +0000

    Add ordering filters to moderation queue

--- ynr/apps/moderation_queue/filters.py
+++ ynr/apps/moderation_queue/filters.py
@@ -2,6 +2,7 @@ import django_filters
 from candidates.models.popolo_extra import Ballot
 from django.forms.widgets import HiddenInput
 from elections.models import Election
+from django.db.models import Max, Case, Value, When, IntegerField
 
 from moderation_queue.models import QueuedImage
 
@@ -33,6 +34,49 @@ class QueuedImageFilter(django_filters.FilterSet):
         label="Filter by current or future election",
     )
 
+    ordering = django_filters.OrderingFilter(
+        choices=[
+            ("-election_date", "Election date candidate is standing in"),
+            (
+                "election_date",
+                "Election date candidate is standing in (oldest first)",
+            ),
+            ("-current_election", "Current election"),
+            ("current_election", "Current election (not current first)"),
+            ("-created", "Date uploaded (newest first)"),
+            ("created", "Date uploaded (oldest first)"),
+        ],
+        fields=[
+            ("election_date", "election_date"),
+            ("current_election", "current_election"),
+            ("created", "created"),
+        ],
+    )
+
     class Meta:
         model = QueuedImage
         fields = ["ballot_paper_id", "election"]
+
+    def __init__(self, *args, **kwargs):
+        """
+        Annotates the querset to ensure that fields used for ordering are
+        annotated.
+        Aggregations are used to avoid duplicate objects being returned when
+        ordering on related fields.
+        """
+        kwargs["queryset"] = kwargs["queryset"].annotate(
+            election_date=Max(
+                "person__memberships__ballot__election__election_date"
+            ),
+            current_election=Max(
+                Case(
+                    When(
+                        person__memberships__ballot__election__current=True,
+                        then=Value(1),
+                    ),
+                    default=0,
+                    output_field=IntegerField(),
+                )
+            ),
+        )
+        super().__init__(*args, **kwargs)
