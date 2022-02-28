ab3b7cb193aba7b8474c8927f5289600788fc391
Michael Collins <michaeljcollinsuk@users.noreply.github.com>
2022-02-28 15:37:50 +0000

    Link from ballot page to moderation queue list

---
+++ ynr/apps/moderation_queue/tests/factories.py
@@ -0,0 +1,10 @@
+import factory
+
+
+class QueuedImageFactory(factory.django.DjangoModelFactory):
+
+    image = factory.django.ImageField()
+    decision = "undecided"
+
+    class Meta:
+        model = "moderation_queue.QueuedImage"
