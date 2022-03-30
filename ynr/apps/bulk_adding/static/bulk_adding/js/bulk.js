de8e0bcf56672356ec1f2db15c0771237bb97842
Sym Roe <sym.roe@talusdesign.co.uk>
2022-03-30 16:09:20 +0100

    Use JS to show and hide extra forms on bulk adding page

--- ynr/apps/bulk_adding/static/bulk_adding/js/bulk.js
+++ ynr/apps/bulk_adding/static/bulk_adding/js/bulk.js
@@ -126,4 +126,53 @@ $(function(){
             $form.remove();
         });
     });
+
+
+    function tbody_is_empty($tbody) {
+      if ($tbody.find("input:not([type=hidden])").val() && $tbody.find("input:not([type=hidden])").val().length > 0) {
+        return false;
+      }
+      if ($tbody.find("option:not([value='']):selected").length > 0) {
+        return false;
+      }
+      return true;
+    }
+
+    // Remove empty fieldsets
+    var always_show_extra = $("form#bulk_add_form")[0].dataset['winnerCount'] * 3;
+    $("#bulk_add_form .sopn_adding_table tbody").each(function(i) {
+      var tbody = $(this);
+      if (tbody_is_empty(tbody)) {
+        if (i >= always_show_extra) {
+          tbody.hide();
+          tbody.prev("thead").hide();
+        }
+      } else {
+        always_show_extra++;
+      }
+    });
+
+
+    // Extra form rows on bulk adding form
+    var $table = $("#bulk_add_form .sopn_adding_table");
+    var add_extra_row = "<tbody class='add_extra_table'><tr><td></td><td><button type='button' id='add_row_button'>Add row</button></td></tr></tbody>";
+    $table.append(add_extra_row);
+    $("#add_row_button").on("click", function() {
+      var total_form_element = $('#id_form-TOTAL_FORMS');
+      var last_tbody = $table.find("tbody:last").prev("tbody");
+      var new_tbody = last_tbody.clone(true, true);
+      var formRegex = RegExp('form-[1-9]+-','g');
+      var new_form_id = $table.find(".ballot_group").length;
+      new_tbody.html(new_tbody.html().replace(formRegex, 'form-'+new_form_id+'-'));
+      var next_form_id = new_form_id += 1;
+      var next_form_label = $table.find("thead:visible:last").find("th").text().match("([0-9]+)")[0];
+      next_form_label = parseInt(next_form_label) + 1;
+      $("<thead><tr><th colspan='3' >Person "+ next_form_label +"</th></tr></thead>").insertBefore($(".add_extra_table"));
+      new_tbody.insertBefore($(".add_extra_table"));
+      new_tbody.find(".select2").remove();
+      populate_party_selects();
+      new_tbody.show();
+      total_form_element.val(next_form_id);
+    });
+
 });
