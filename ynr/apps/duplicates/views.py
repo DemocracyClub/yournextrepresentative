167664f63c8d800e049c653f7da3e0fc5eebae69
Sym Roe <sym.roe@talusdesign.co.uk>
2022-04-18 12:19:01 +0100

    Optimize queries for duplicates list view

--- ynr/apps/duplicates/views.py
+++ ynr/apps/duplicates/views.py
@@ -1,9 +1,11 @@
 from django.contrib import messages
+from django.db.models import Prefetch
 from django.views.generic import ListView, UpdateView
 from django.urls import reverse_lazy
 
 from auth_helpers.views import GroupRequiredMixin
 from candidates.models.auth import TRUSTED_TO_MERGE_GROUP_NAME
+from popolo.models import Membership
 
 from .models import DuplicateSuggestion
 from .forms import RejectionForm
@@ -17,7 +19,22 @@ class DuplicateSuggestionListView(GroupRequiredMixin, ListView):
         """
         Only display suggestions that are open
         """
-        return DuplicateSuggestion.objects.open()
+        return (
+            DuplicateSuggestion.objects.open()
+            .select_related("person", "other_person", "user")
+            .prefetch_related(
+                Prefetch(
+                    "person__memberships",
+                    Membership.objects.all().select_related("ballot", "party"),
+                ),
+                Prefetch(
+                    "other_person__memberships",
+                    Membership.objects.all().select_related("ballot", "party"),
+                ),
+                "person__other_names",
+                "other_person__other_names",
+            )
+        )
 
 
 class RejectSuggestion(GroupRequiredMixin, UpdateView):
