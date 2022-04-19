7e8ffa2a47f79fe8b1e787fafab5afcaac468399
Sym Roe <sym.roe@talusdesign.co.uk>
2022-04-19 09:22:23 +0100

    Page search results

--- ynr/apps/search/views.py
+++ ynr/apps/search/views.py
@@ -1,15 +1,16 @@
 from django.http import HttpResponseRedirect
 from django.urls import reverse
-from django.views.generic import TemplateView
+from django.views.generic import ListView
 
 from search.utils import search_person_by_name
 from search.forms import PersonSearchForm
 from elections.uk.lib import is_valid_postcode
 
 
-class PersonSearch(TemplateView):
+class PersonSearch(ListView):
     template_name = "people/search/search.html"
     form_class = PersonSearchForm
+    paginate_by = 20
 
     def get(self, request, *args, **kwargs):
         ret = super().get(request, *args, **kwargs)
@@ -26,11 +27,14 @@ class PersonSearch(TemplateView):
 
         return ret
 
+    def get_queryset(self):
+        return search_person_by_name(
+            self.request.GET.get("q", ""), synonym=True
+        )
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["query"] = self.request.GET.get("q", "")
         context["looks_like_postcode"] = is_valid_postcode(context["query"])
-        if context["query"]:
-            context["results"] = search_person_by_name(context["query"])[:10]
 
         return context
