ae5913123d0f65b5519acce59a94f4e356166891
Michael Collins <michaeljcollinsuk@users.noreply.github.com>
2022-05-04 16:39:31 +0100

    Raise 404 when accessing results form if polls open

--- ynr/apps/uk_results/tests/test_smoke_test_views.py
+++ ynr/apps/uk_results/tests/test_smoke_test_views.py
@@ -1,10 +1,15 @@
 from django.test import TestCase
 from django.urls import reverse
 from django_webtest import WebTest
+from freezegun import freeze_time
 from candidates.models.popolo_extra import Ballot
 
 from candidates.tests.auth import TestUserMixin
-from candidates.tests.factories import MembershipFactory
+from candidates.tests.factories import (
+    BallotPaperFactory,
+    ElectionFactory,
+    MembershipFactory,
+)
 from candidates.tests.uk_examples import UK2015ExamplesMixin
 from people.tests.factories import PersonFactory
 from uk_results.models import CandidateResult, ResultSet
@@ -77,3 +82,45 @@ class TestUKResults(TestUserMixin, UK2015ExamplesMixin, WebTest, TestCase):
             url, user=self.user_who_can_record_results, expect_errors=True
         )
         self.assertEqual(resp.status_code, 404)
+
+    @freeze_time("2022-05-05 10:00:00")
+    def test_form_view_polls_open(self):
+        election = ElectionFactory(
+            election_date="2022-05-05",
+            slug="local.sheffield.2022-05-05",
+            current=False,
+        )
+        ballot = BallotPaperFactory(
+            election=election,
+            ballot_paper_id="local.sheffield.2022-05-05",
+            voting_system="FPTP",
+        )
+        url = reverse(
+            "ballot_paper_results_form",
+            kwargs={"ballot_paper_id": ballot.ballot_paper_id},
+        )
+        resp = self.app.get(
+            url, user=self.user_who_can_record_results, expect_errors=True
+        )
+        self.assertEqual(resp.status_code, 404)
+
+    @freeze_time("2022-05-05 22:00:00")
+    def test_form_view_polls_closed(self):
+        election = ElectionFactory(
+            election_date="2022-05-05",
+            slug="local.sheffield.2022-05-05",
+            current=False,
+        )
+        ballot = BallotPaperFactory(
+            election=election,
+            ballot_paper_id="local.sheffield.2022-05-05",
+            voting_system="FPTP",
+        )
+        url = reverse(
+            "ballot_paper_results_form",
+            kwargs={"ballot_paper_id": ballot.ballot_paper_id},
+        )
+        resp = self.app.get(
+            url, user=self.user_who_can_record_results, expect_errors=True
+        )
+        self.assertEqual(resp.status_code, 200)
--- ynr/apps/uk_results/views/base_views.py
+++ ynr/apps/uk_results/views/base_views.py
@@ -2,7 +2,7 @@ import csv
 from datetime import date
 
 from braces.views import LoginRequiredMixin
-from django.http import HttpResponse
+from django.http import Http404, HttpResponse
 from django.shortcuts import get_object_or_404
 from django.views.generic import FormView, TemplateView
 
@@ -37,6 +37,12 @@ class BallotPaperResultsUpdateView(LoginRequiredMixin, FormView):
             voting_system=Ballot.VOTING_SYSTEM_FPTP,
             ballot_paper_id=self.kwargs["ballot_paper_id"],
         )
+
+        if not self.ballot.polls_closed:
+            raise Http404(
+                f"Polls are still open for {self.ballot.ballot_paper_id}. Please check back after polls close at 10pm."
+            )
+
         try:
             kwargs["instance"] = self.ballot.resultset
         except:
