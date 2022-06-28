ee444fe9ee45e1a2ac09033f5e15d6e9956885db
Virginia Dooley <vcdooley@gmail.com>
2022-06-28 18:21:31 +0100

    Test recent changes filters

--- ynr/apps/candidates/tests/test_recent_changes_view.py
+++ ynr/apps/candidates/tests/test_recent_changes_view.py
@@ -42,3 +42,43 @@ class TestRecentChangesView(TestUserMixin, WebTest):
         response = self.app.get("/recent-changes")
         table = response.html.find("table")
         self.assertEqual(5, len(table.find_all("tr")))
+
+
+class TestLoggedActionRecentChangesFilter(TestUserMixin, WebTest):
+    def setUp(self):
+        self.person = people.tests.factories.PersonFactory.create(
+            id=2233, name="Joyce Byers"
+        )
+
+    def test_filters(self):
+        response = self.app.get(
+            "/person/{id}/update".format(id=self.person.id), user=self.user
+        )
+        form = response.forms["person-details"]
+        form["name"] = "Joyce Myers"
+        form["source"] = "www.google.com"
+        response = form.submit()
+
+        self.person.refresh_from_db()
+        self.assertEqual(self.person.name, "Joyce Myers")
+        version = self.person.versions[0]  # Latest version
+        self.assertEqual(version["information_source"], "www.google.com")
+
+        response = self.app.get(
+            "/person/{id}/update".format(id=self.person.id), user=self.user
+        )
+        form["gender"] = "female"
+        form["source"] = "www.twitter.com"
+        form.submit()
+
+        self.person.refresh_from_db()
+        self.assertEqual(self.person.gender, "female")
+        version = self.person.versions[0]
+        self.assertEqual(version["information_source"], "www.twitter.com")
+
+        self.assertEqual(2, len(self.person.versions))
+
+        response = self.app.get("/recent-changes?version_fields=name")
+        table = response.html.find("table")
+        self.assertEqual(3, len(table.find_all("tr")))
+        self.assertContains(response, "Person updated")
