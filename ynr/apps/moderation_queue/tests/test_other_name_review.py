1033da5c9049f93347da4bd6395ade56801bdbc7
Sym Roe <sym.roe@talusdesign.co.uk>
2023-04-04 14:11:30 +0100

    Use hidden field on form
    This makes the form easier to manage and more robust to text changes.
    
    Also add some tests to verify it's working

---
+++ ynr/apps/moderation_queue/tests/test_other_name_review.py
@@ -0,0 +1,54 @@
+from django.contrib.auth.models import User, Group
+from django.urls import reverse
+from django_webtest import WebTest
+
+from candidates.tests.auth import TestUserMixin
+from candidates.tests.factories import MembershipFactory
+from candidates.tests.uk_examples import UK2015ExamplesMixin
+from people.models import TRUSTED_TO_EDIT_NAME
+from people.tests.factories import PersonFactory
+from popolo.models import OtherName
+
+
+class OtherNameReviewTests(TestUserMixin, UK2015ExamplesMixin, WebTest):
+    def setUp(self):
+        super().setUp()
+        self.person = PersonFactory.create(id=2009, name="Tessa Jowell")
+        self.other_name = self.person.other_names.create(
+            name="Tessa Palmer", needs_review=True
+        )
+        MembershipFactory.create(
+            person=self.person,
+            post=self.dulwich_post,
+            party=self.labour_party,
+            ballot=self.dulwich_post_ballot,
+        )
+        self.group = Group.objects.get(name=TRUSTED_TO_EDIT_NAME)
+        self.user_who_can_name_review: User = self.user
+        self.user_who_can_name_review.groups.add(self.group)
+
+    def test_name_edit_page_approve(self):
+        resp = self.app.get(
+            reverse("person-name-review"), user=self.user_who_can_name_review
+        )
+        self.assertEqual(resp.status_code, 200)
+        self.assertContains(resp, "Approve")
+
+        form = resp.forms[f"review_name_form_{self.other_name.pk}"]
+        form.submit(name="decision_approve")
+        self.person.refresh_from_db()
+        self.assertEqual(self.person.name, "Tessa Palmer")
+        self.assertFalse(OtherName.objects.filter(needs_review=True).exists())
+
+    def test_name_edit_page_reject(self):
+        resp = self.app.get(
+            reverse("person-name-review"), user=self.user_who_can_name_review
+        )
+        self.assertEqual(resp.status_code, 200)
+        self.assertContains(resp, "Reject")
+
+        form = resp.forms[f"review_name_form_{self.other_name.pk}"]
+        form.submit(name="decision_reject")
+        self.person.refresh_from_db()
+        self.assertEqual(self.person.name, "Tessa Jowell")
+        self.assertFalse(OtherName.objects.filter(needs_review=True).exists())
