2d01c0d4b116c77d9c3f16609e2147bc34dca6c1
Virginia Dooley <vcdooley@gmail.com>
2023-04-19 14:19:23 +0100

    Get the queued_image from s3 storage

--- ynr/apps/moderation_queue/management/commands/moderation_queue_rotate_images.py
+++ ynr/apps/moderation_queue/management/commands/moderation_queue_rotate_images.py
@@ -1,14 +1,9 @@
 import json
 import sorl
+from PIL import Image
 from django.core.management.base import BaseCommand, CommandError
 from moderation_queue.models import QueuedImage
-from PIL import Image
-
-# These magic values are because the AWS API crops faces quite tightly by
-# default, meaning we literally just get the face. These values are about
-# right or, they are more right than the default crop.
-MIN_SCALING_FACTOR = 0.7
-MAX_SCALING_FACTOR = 1.3
+from django.core.files.storage import default_storage
 
 
 class Command(BaseCommand):
@@ -20,13 +15,16 @@ class Command(BaseCommand):
         )
 
         for qi in qs:
+            queued_image = default_storage.open(qi.image.name, "rb")
             try:
                 detected = json.loads(qi.detection_metadata)
                 if len(detected["FaceDetails"]) > 0:
-                    self.rotate_queued_images(image=qi.image, detected=detected)
-                    sorl.thumbnail.delete(qi.image.name, delete_file=False)
+                    self.rotate_queued_images(
+                        image=queued_image, detected=detected
+                    )
+                    sorl.thumbnail.delete(queued_image.name, delete_file=False)
                 else:
-                    msg = "No face details found for image: {qi.image.id}"
+                    msg = "No face details found for image: {queued_image.id}"
                     self.stdout.write(msg)
             except Exception as e:
                 msg = "Skipping QueuedImage{id}: {error}"
@@ -40,7 +38,7 @@ class Command(BaseCommand):
         """
         Detects the rotation of an image and returns an integer
         """
-        image_path = image.path
+        image_path = image.name
         image = Image.open(image, formats=None)
         # Calculate the angle image is rotated by rounding the roll
         # value to the nearest multiple of 90
