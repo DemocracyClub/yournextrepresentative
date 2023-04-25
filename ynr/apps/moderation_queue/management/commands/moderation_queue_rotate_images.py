d641389ca25995a2219f279d8303d52e01ddf492
Virginia Dooley <vcdooley@gmail.com>
2023-04-25 13:55:46 +0100

    Refactor rotate management command

--- ynr/apps/moderation_queue/management/commands/moderation_queue_rotate_images.py
+++ ynr/apps/moderation_queue/management/commands/moderation_queue_rotate_images.py
@@ -1,3 +1,4 @@
+from io import BytesIO
 import json
 import sorl
 from django.core.management.base import BaseCommand, CommandError
@@ -24,7 +25,6 @@ class Command(BaseCommand):
                 detected = json.loads(qi.detection_metadata)
                 if len(detected["FaceDetails"]) > 0:
                     self.rotate_queued_images(image=qi.image, detected=detected)
-                    sorl.thumbnail.delete(qi.image.name, delete_file=False)
                 else:
                     msg = "No face details found for image: {qi.image.id}"
                     self.stdout.write(msg)
@@ -41,12 +41,15 @@ class Command(BaseCommand):
         Detects the rotation of an image and returns an integer
         """
         image_path = image.path
-        image = Image.open(image, formats=None)
+        queued_image = Image.open(image, formats=None)
         # Calculate the angle image is rotated by rounding the roll
         # value to the nearest multiple of 90
         roll = detected["FaceDetails"][0]["Pose"]["Roll"]
         img_rotation_angle = round(roll / 90) * 90
-        rotated = image.rotate(angle=img_rotation_angle, expand=True)
+        rotated = queued_image.rotate(angle=img_rotation_angle, expand=True)
+        buffer = BytesIO()
         rotated.save(image_path, "PNG")
-        image.close()
-        return rotated
+        queued_image.image.save(queued_image.image.name, buffer)
+        queued_image.rotation_tried = True
+        sorl.thumbnail.delete(queued_image.image.name, delete_file=False)
+        return queued_image
