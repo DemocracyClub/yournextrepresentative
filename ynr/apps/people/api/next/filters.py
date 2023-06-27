6ad1420507434704228fa4a9e9b7345d5b48c5ec
Sym Roe <sym.roe@talusdesign.co.uk>
2023-06-27 22:30:40 +0100

    Add PersonIdentifier filter to person endpoint

--- ynr/apps/people/api/next/filters.py
+++ ynr/apps/people/api/next/filters.py
@@ -1,3 +1,5 @@
+import django_filters
+
 from people.models import Person
 from api.next.filters import LastUpdatedMixin
 
@@ -6,3 +8,8 @@ class PersonFilter(LastUpdatedMixin):
     class Meta:
         model = Person
         fields = ["last_updated"]
+
+    has_identifier = django_filters.CharFilter(
+        field_name="tmp_person_identifiers__value_type",
+        help_text="Filter by the existence of an identifier type",
+    )
