edcc8af028be81fc30fa8561bd3521d59e1bc1aa
Sym Roe <sym.roe@talusdesign.co.uk>
2023-10-17 15:12:18 +0100

    MVP "my profile" page with API key and recent edits

--- ynr/apps/wombles/urls.py
+++ ynr/apps/wombles/urls.py
@@ -1,8 +1,9 @@
-from django.urls import re_path
+from django.urls import path, re_path
 
-from .views import SingleWombleView, WombleTagsView, WombleTagView
+from .views import MyProfile, SingleWombleView, WombleTagsView, WombleTagView
 
 urlpatterns = [
+    path("me", MyProfile.as_view(), name="my_profile"),
     re_path(r"^tags/$", WombleTagsView.as_view(), name="womble_tags"),
     re_path(
         r"^tags/(?P<tag>[^/]+)/$", WombleTagView.as_view(), name="womble_tag"
