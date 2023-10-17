fe44f4b7e2fd45c130588756334a3836271c01db
Sym Roe <sym.roe@talusdesign.co.uk>
2023-10-17 14:46:58 +0100

    Add API keys and give every user a key

--- ynr/apps/api/apps.py
+++ ynr/apps/api/apps.py
@@ -1,5 +1,17 @@
 from django.apps import AppConfig
+from django.conf import settings
+from django.db.models.signals import post_save
 
 
 class ApiConfig(AppConfig):
     name = "api"
+
+    def ready(self):
+        post_save.connect(create_auth_token, sender=settings.AUTH_USER_MODEL)
+
+
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        from rest_framework.authtoken.models import Token
+
+        Token.objects.create(user=instance)
