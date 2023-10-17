cada9a826c8cc5c3a0db0ff5ae802df1d1fa892d
Sym Roe <sym.roe@talusdesign.co.uk>
2023-10-17 14:12:46 +0100

    Reduce per-page to 100
    This should reduce the load on the server of getting more rows

--- ynr/apps/api/next/views.py
+++ ynr/apps/api/next/views.py
@@ -22,7 +22,7 @@ def parse_date(date_text):
 class ResultsSetPagination(pagination.PageNumberPagination):
     page_size = 10
     page_size_query_param = "page_size"
-    max_page_size = 200
+    max_page_size = 100
 
 
 class OrganizationViewSet(viewsets.ReadOnlyModelViewSet):
