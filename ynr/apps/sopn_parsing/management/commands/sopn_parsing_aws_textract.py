7aba2c50b56d1f8c371d1659277a760e6c02424e
Virginia Dooley <vcdooley@gmail.com>
2023-11-14 13:29:29 +0000

    Convert AWSTextract response to panda df

--- ynr/apps/sopn_parsing/management/commands/sopn_parsing_aws_textract.py
+++ ynr/apps/sopn_parsing/management/commands/sopn_parsing_aws_textract.py
@@ -1,15 +1,19 @@
 import os
+from io import StringIO
 from time import sleep
 
 import boto3
 from django.core.management.base import BaseCommand
-from official_documents.models import TextractResult
+from official_documents.models import OfficialDocument, TextractResult
+from sopn_parsing.models import AWSTextractParsedSOPN
 
-# this is an html saved as a pdf
-# test_sopn = "https://www.bury.gov.uk/council-and-democracy/elections-and-voting/statement-of-persons-nominated/"
-test_sopn = (
-    "ynr/apps/sopn_parsing/management/commands/test_sopns/HackneySOPN.pdf"
-)
+# # this is a single page pdf SoPN for testing
+# test_sopn = (
+#     "ynr/apps/sopn_parsing/management/commands/test_sopns/HackneySOPN.pdf"
+# )
+
+# # this is a multipage page html saved as pdf SoPN for testing
+test_sopn = "ynr/apps/sopn_parsing/management/commands/test_sopns/BurySOPN.pdf"
 
 accepted_file_types = [
     ".pdf",
@@ -20,6 +24,7 @@ accepted_file_types = [
     ".tiff",
 ]
 
+
 s3 = boto3.client("s3")
 textract_client = boto3.client("textract")
 session = boto3.session.Session(
@@ -27,6 +32,7 @@ session = boto3.session.Session(
     aws_secret_access_key=os.environ.get("AWS_SECRET_ACCESS_KEY"),
     aws_session_token=os.environ.get("AWS_SECURITY_TOKEN"),
 )
+official_document = OfficialDocument.objects.get(id=1)
 
 
 class Command(BaseCommand):
@@ -75,64 +81,104 @@ class Command(BaseCommand):
         """This is Step 2 of the SOPN parsing process using AWS Textract.
         https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/textract/client/get_document_analysis.html#
         """
-        response = textract_client.get_document_analysis(JobId=job_id)
-        ## this is a temp approach to step 2 to get the job id for dev env
-        while response["JobStatus"] not in ["SUCCEEDED", "FAILED"]:
+        textract_result = textract_client.get_document_analysis(JobId=job_id)
+        while textract_result["JobStatus"] not in ["SUCCEEDED", "FAILED"]:
             sleep(5)
-            response = textract_client.get_document_analysis(JobId=job_id)
-
-        if response["JobStatus"] == "SUCCEEDED":
-            # how do we ensure that the upload is unique if the
-            # job id is unique with every upload? this won't likely
-            # be an issue in production as we'll be uploading
-            # files from the s3 bucket
-            textract_result = TextractResult.objects.create(
-                job_id=job_id,
-                json_response=response,
+            textract_result = textract_client.get_document_analysis(
+                JobId=job_id
             )
-            textract_result.save()
+
+        if textract_result["JobStatus"] == "SUCCEEDED":
+            TextractResult.objects.update_or_create(
+                official_document=official_document,
+                defaults={"job_id": job_id, "json_response": textract_result},
+            )
             print(f"Job succeeded:{job_id}")
             print(
                 "The number of pages in this document is:",
-                response["DocumentMetadata"]["Pages"],
+                textract_result["DocumentMetadata"]["Pages"],
             )
-            self.parse_aws_textract_response(response)
-        elif response["JobStatus"] == "FAILED":
+            self.transform_response_into_df(textract_result)
+        elif textract_result["JobStatus"] == "FAILED":
             print(f"Job failed:{job_id}")
-            print(response["StatusMessage"])
+            print(textract_result["StatusMessage"])
         else:
             print(f"Job {job_id} still running")
 
-    def parse_aws_textract_response(self, response):
-        """This method takes a response from AWS Textract
-        and parses the data into a table. The alternative
-        to this aproach would be the use of SNS to send
-        the response to a lambda function which would then
-        parse the data into a table."""
-        from trp import Document
+    def map_blocks(self, blocks, block_type):
+        return {
+            block["Id"]: block
+            for block in blocks
+            if block["BlockType"] == block_type
+        }
 
-        doc = Document(response)
+    def get_children_ids(self, block):
+        for rels in block.get("Relationships", []):
+            if rels["Type"] == "CHILD":
+                yield from rels["Ids"]
 
-        for page in doc.pages:
-            for page in doc.pages:
-                for table in page.tables:
-                    for r, row in enumerate(table.rows):
-                        for c, cell in enumerate(row.cells):
-                            if "Candidate" in cell.text:
-                                candidate_column = c
-                            if "Description" in cell.text:
-                                description_column = c
-                                for r, row in enumerate(table.rows):
-                                    for c, cell in enumerate(row.cells):
-                                        if c == candidate_column:
-                                            candidates = []
-                                            candidates.append(cell.text)
-                                        if c == description_column:
-                                            descriptions = []
-                                            descriptions.append(cell.text)
-                                            for candidate, description in zip(
-                                                candidates, descriptions
-                                            ):
-                                                print(
-                                                    f"{candidate}: {description}"
-                                                )
+    def transform_response_into_df(self, textract_result):
+        """This step is to transform the raw data into a pandas dataframe so
+        that we can then weave it into the existing parsing process.
+        before we try to match parties."""
+        # with credit to https://maxhalford.github.io/blog/textract-table-to-pandas/
+        # keep an eye on this as it may be useful: https://pypi.org/project/amazon-textract-response-parser/
+        # for now, we're just going to use the raw data
+        import pandas as pd
+
+        blocks = textract_result["Blocks"]
+        tables = self.map_blocks(blocks, "TABLE")
+        cells = self.map_blocks(blocks, "CELL")
+        words = self.map_blocks(blocks, "WORD")
+        selections = self.map_blocks(blocks, "SELECTION_ELEMENT")
+        lines = self.map_blocks(blocks, "LINE")
+
+        dataframes = []
+
+        if tables:
+            # if there are tables, we know this is a structured document
+            for table in tables.values():
+                # Determine all the cells that belong to this table
+                table_cells = [
+                    cells[cell_id] for cell_id in self.get_children_ids(table)
+                ]
+
+                # Determine the table's number of rows and columns
+                n_rows = max(cell["RowIndex"] for cell in table_cells)
+                n_cols = max(cell["ColumnIndex"] for cell in table_cells)
+                content = [[None for _ in range(n_cols)] for _ in range(n_rows)]
+
+                # Fill in each cell
+                for cell in table_cells:
+                    cell_contents = [
+                        words[child_id]["Text"]
+                        if child_id in words
+                        else selections[child_id]["SelectionStatus"]
+                        for child_id in self.get_children_ids(cell)
+                    ]
+                    i = cell["RowIndex"] - 1
+                    j = cell["ColumnIndex"] - 1
+                    content[i][j] = " ".join(cell_contents)
+
+                dataframe = pd.DataFrame(content[1:], columns=content[0])
+                dataframes.append(dataframe)
+        elif lines:
+            print(
+                "No tables found. This may be an html SoPN. Let's try to parse the lines."
+            )
+            sopn_string_list = []
+            for block in blocks:
+                if block["BlockType"] == "LINE":
+                    sopn_string_list.append(block["Text"].replace("\n", ","))
+            sopn_string = " ".join(sopn_string_list)
+            string_io = StringIO(sopn_string)
+            dataframe = pd.read_csv(string_io)
+        else:
+            pass
+        AWSTextractParsedSOPN.objects.create(
+            sopn=official_document,
+            raw_data=dataframe.to_json(),
+            raw_data_type="pandas",
+            parsed_data=dataframe.to_json(),
+            status="parsed",
+        )
