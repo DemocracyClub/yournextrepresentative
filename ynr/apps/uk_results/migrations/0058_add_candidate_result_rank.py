6a8508b31cf81366c3d37165342fc0c62009c9db
Virginia Dooley <vcdooley@gmail.com>
2024-01-15 17:14:46 +0000

    fixup! Add candidate result rank where rank is None

--- ynr/apps/uk_results/migrations/0058_add_candidate_result_rank.py
+++ ynr/apps/uk_results/migrations/0058_add_candidate_result_rank.py
@@ -2,14 +2,27 @@ from django.db import migrations
 
 
 def get_candidate_rank(apps, schema_editor):
-    candidate_results = apps.get_model("uk_results", "CandidateResult")
-    results = candidate_results.objects.filter(rank=None).order_by(
-        "-num_ballots"
-    )
-    for index, result in enumerate(results):
-        index = index + 1
-        result.rank = index
-        result.save()
+    """Only update the rank for the candidate results in a
+    given resultset. It's possible that a resultset exists
+    without any candidate results, so we need to check for
+    that first.
+    """
+    candidate_result = apps.get_model("uk_results", "CandidateResult")
+    resultsets = apps.get_model("uk_results", "ResultSet")
+    for resultset in resultsets.objects.all():
+        candidate_results = candidate_result.objects.filter(
+            result_set=resultset
+        ).order_by("-num_ballots")
+
+        if candidate_results.exists():
+            for i, cr in enumerate(candidate_results):
+                if cr.rank is None:
+                    cr.rank = i + 1
+                    cr.save()
+                else:
+                    pass
+        else:
+            pass
 
 
 class Migration(migrations.Migration):
