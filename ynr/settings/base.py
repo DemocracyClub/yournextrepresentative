2d48aa8c6957ab9c8bbadd7400d03ec7f659aea9
Virginia Dooley <vcdooley@gmail.com>
2024-01-15 10:28:32 +0000

    USE_L1ON is deprecated; now built in

--- ynr/settings/base.py
+++ ynr/settings/base.py
@@ -224,7 +224,6 @@ LANGUAGES = [("en-gb", "British English")]
 LANGUAGE_CODE = "en-gb"
 TIME_ZONE = "Europe/London"
 USE_I18N = False
-USE_L10N = True
 USE_TZ = True
 DD_MM_DATE_FORMAT_PREFERRED = True
 
