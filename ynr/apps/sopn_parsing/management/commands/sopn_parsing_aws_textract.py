03c1b8a6985a96ed66b75d10beb3cce3026b7234
Virginia Dooley <vcdooley@gmail.com>
2024-02-01 11:17:19 +0000

    Handle throttled AWS calls on open jobs

--- ynr/apps/sopn_parsing/management/commands/sopn_parsing_aws_textract.py
+++ ynr/apps/sopn_parsing/management/commands/sopn_parsing_aws_textract.py
@@ -1,4 +1,8 @@
-from official_documents.models import OfficialDocument
+from datetime import timedelta
+from time import sleep
+
+from django.utils import timezone
+from official_documents.models import OfficialDocument, TextractResult
 from sopn_parsing.helpers.command_helpers import BaseSOPNParsingCommand
 from sopn_parsing.helpers.extract_pages import (
     TextractSOPNHelper,
@@ -25,16 +29,31 @@ class Command(BaseSOPNParsingCommand):
             help="Get AWS Textract results for each SOPN",
         )
 
+    def check_queue_length(self):
+        TEXTRACT_STAT_JOBS_PER_SECOND_QUOTA = 0.5
+        sleep(TEXTRACT_STAT_JOBS_PER_SECOND_QUOTA)
+
+        time_window = timezone.now() - timedelta(hours=1)
+        processing = TextractResult.objects.filter(
+            created__gt=time_window, analysis_status="NOT_STARTED"
+        )
+        print(f"Processing: {processing.count()}")
+        TEXTRACT_CONCURRENT_QUOTA = 90  # move to settings
+        if processing.count() > TEXTRACT_CONCURRENT_QUOTA:
+            sleep(60)
+
     def handle(self, *args, **options):
         queryset = self.get_queryset(options)
         if options["start_analysis"]:
             for ballot in queryset:
+                self.check_queue_length()
                 official_documents = OfficialDocument.objects.filter(
                     ballot=ballot
                 )
                 official_document = official_documents.last()
                 textract_helper = TextractSOPNHelper(official_document)
                 # TO DO: add logging here
+                sleep(0.3)
                 textract_helper.start_detection(official_document)
         if options["get_results"]:
             for ballot in queryset:
