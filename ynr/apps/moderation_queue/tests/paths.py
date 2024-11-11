39beabca2bc56c48743d9a7d106cc5c13801854d
Sym Roe <sym.roe@talusdesign.co.uk>
2024-11-11 17:42:32 +0000

    Remove alpha from emblems when importing from the EC

--- ynr/apps/moderation_queue/tests/paths.py
+++ ynr/apps/moderation_queue/tests/paths.py
@@ -4,6 +4,7 @@ EXAMPLE_IMAGE_FILENAME = abspath(join(dirname(__file__), "example-image.jpg"))
 BROKEN_IMAGE_FILENAME = abspath(
     join(dirname(__file__), "broke-image-example.htm")
 )
+IMAGE_WITH_ALPHA = abspath(join(dirname(__file__), "image-with-alpha.png"))
 
 ROTATED_IMAGE_FILENAME = abspath(
     join(dirname(__file__), "rotated_photo_with_exif.jpg")
