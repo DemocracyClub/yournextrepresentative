caf9aa281a512f17d07788b3b6abe875fa26a85f
Jonathan Matthews <github@hello.jonathanmatthews.com>
2024-12-30 10:59:42 +0000

    scripts: add podman-compose-exec shim
    This adds a script that shims "podman compose exec", and changes the renamed
    shim script that invokes Django management commands to use it.
    
    It also removes a couple of envvars from the compose file which are more
    properly situated in the gitignored, per-developer env/frontend.env file.

---
+++ scripts/container.exec.bash
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# container.exec.bash invokes a command in a "frontend" container.
+# The container must be running before running this script (see README.md).
+#
+# Usage:
+#    scripts/container.exec.bash echo hello world 
+#
+# This script is a deliberately simple convenience shim, and doesn't include a
+# way to set or override environment variables. To do this, invoke "podman"
+# directly:
+#     podman compose exec -e key1=val -e key2=val frontend command param1 param2
+# Updating variables held in env/frontend.env has no effect until the running
+# container is restarted.
+
+# The command being invoked does not need to be quoted, unless it contains
+# shell meta-characters or similar. Multiple words are fine, without quotes.
+command="$@"
+
+# Change to the directory above the directory containing this script.
+cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/.."
+
+podman compose exec frontend $command
