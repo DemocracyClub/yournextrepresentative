1917bd5bdfc8829b146bed1464693e7a7894f67d
Jonathan Matthews <github@hello.jonathanmatthews.com>
2025-02-11 11:02:11 +0000

    deploy/cdk: use AWS CDK L3 construct
    This WIP is used to stand up a Fargate-backed ECS service manually, using the
    AWS ecsPatterns.ApplicationLoadBalancedFargateService L3 construct. This is in
    order to discover the daily cost of the deployment now that NAT Gateways have
    been eliminated, so that a decision can be made about using the L3 construct as
    the basis for YNR's deployment, versus using a lower level CDK abstraction.

--- deploy/cdk/stack/ynr.py
+++ deploy/cdk/stack/ynr.py
@@ -1,11 +1,43 @@
 from aws_cdk import Stack
-from aws_cdk import aws_s3 as s3
+from aws_cdk import aws_ec2 as ec2
+from aws_cdk import aws_ecs as ecs
+from aws_cdk import aws_ecs_patterns as ecs_patterns
 from constructs import Construct
 
 
 class YnrStack(Stack):
-
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        bucket = s3.Bucket(self, 'test')  # noqa: F841
+        publicSubnet = ec2.SubnetConfiguration(
+            name="Public", subnet_type=ec2.SubnetType.PUBLIC, cidr_mask=24
+        )
+
+        vpc = ec2.Vpc(
+            self,
+            "YnrVpc",
+            enable_dns_hostnames=False,
+            enable_dns_support=True,
+            create_internet_gateway=True,
+            max_azs=2,
+            nat_gateways=0,
+            subnet_configuration=[publicSubnet],
+        )
+
+        cluster = ecs.Cluster(self, "YnrCluster", vpc=vpc)
+
+        ecs_patterns.ApplicationLoadBalancedFargateService(
+            self,
+            "YnrService",
+            cluster=cluster,
+            assign_public_ip=True,
+            cpu=512,
+            memory_limit_mib=1024,
+            desired_count=2,
+            task_image_options=ecs_patterns.ApplicationLoadBalancedTaskImageOptions(
+                image=ecs.ContainerImage.from_registry(
+                    "amazon/amazon-ecs-sample"
+                )
+            ),
+            public_load_balancer=True,
+        )
