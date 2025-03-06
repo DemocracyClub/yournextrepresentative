1106f9ca1b01ea480ba4842813b8c6c3e2e5e5ac
Matt Peperell <mattp@mlemblep.com>
2025-03-06 12:12:00 +0000

    deploy: Read secrets from Parameter Store

--- deploy/cdk/app.py
+++ deploy/cdk/app.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python3
 
+import os
+
 import aws_cdk as cdk
 from stack.ynr import YnrStack
 
 app = cdk.App()
-YnrStack(app, "YnrStack",
-    # Uncomment the next line to specialize this stack for the AWS Account
-    # and Region that are implied by the current CLI configuration.
-    #env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
-    )
+YnrStack(
+    app,
+    "YnrStack",
+    env=cdk.Environment(
+        account=os.getenv("CDK_DEFAULT_ACCOUNT"), region=os.getenv("CDK_DEFAULT_REGION")
+    ),
+)
 
 app.synth()
