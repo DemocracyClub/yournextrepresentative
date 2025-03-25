1a1e5896a84c85c6e21912a42a91ff16590a4902
awdem <wdemontmollin@gmail.com>
2025-03-25 16:07:40 +0000

    add existing pid validation to PersonIdentifierField

--- ynr/apps/bulk_adding/fields.py
+++ ynr/apps/bulk_adding/fields.py
@@ -1,5 +1,36 @@
 from django import forms
+from django.core.exceptions import ValidationError
+from django.core.validators import URLValidator, validate_email
+from people.helpers import (
+    clean_instagram_url,
+    clean_linkedin_url,
+    clean_mastodon_username,
+    clean_twitter_username,
+    clean_wikidata_id,
+)
 from people.models import PersonIdentifier
+
+HTTP_IDENTIFIERS = [
+    "homepage_url",
+    "facebook_personal_url",
+    "party_ppc_page_url",
+    "linkedin_url",
+    "facebook_page_url",
+    "wikipedia_url",
+    "blue_sky_url",
+    "threads_url",
+    "other_url",
+    "tiktok_url",
+]
+
+
+def clean_email(email):
+    validate_email(email)
+    if email.lower().endswith("parliament.uk"):
+        raise ValueError(
+            "MPs are locked out of their email addresses during a general election. Please find a different email address."
+        )
+    return email
 
 
 class PersonIdentifierWidget(forms.MultiWidget):
@@ -18,7 +49,9 @@ class PersonIdentifierWidget(forms.MultiWidget):
         )
 
     def decompress(self, value):
-        # TODO: Turn single value back into list of two values
+        if value:
+            pid_type, pid = next(iter(value.items()))
+            return [pid, pid_type]
         return [None, None]
 
 
@@ -50,7 +83,30 @@ class PersonIdentifierField(forms.MultiValueField):
         )
 
     def compress(self, data_list):
-        # TODO:
-        # Validations
-        # if valid, create single value
-        return " ".join(data_list)
+        pid = data_list[0]
+        pid_type = data_list[1]
+        # Validate URLs:
+        if pid_type in HTTP_IDENTIFIERS:
+            # Add https schema if missing
+            if not pid.startswith("http"):
+                pid = f"https://{pid}"
+            URLValidator()(pid)
+
+        # Validate pid:
+        try:
+            if pid_type == "instagram_url":
+                clean_instagram_url(pid)
+            elif pid_type == "linkedin_url":
+                clean_linkedin_url(pid)
+            elif pid_type == "mastodon_url":
+                clean_mastodon_username(pid)
+            elif pid_type == "twitter_url":
+                clean_twitter_username(pid)
+            elif pid_type == "wikidata_id":
+                clean_wikidata_id(pid)
+            elif pid_type == "email":
+                clean_email(pid)
+        except ValueError as e:
+            raise ValidationError(e)
+
+        return {pid_type: pid}
