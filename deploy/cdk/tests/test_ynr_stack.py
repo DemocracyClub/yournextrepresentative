DemocracyClub/yournextrepresentative

commit 23f8d486103ab4368a6a73d8d454e7f7cf85ec30

Create the app in an existing VPC
We look up the VPC from the parameter store

deploy/cdk/stack/ynr.py
@@ -11,19 +11,9 @@ class YnrStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        publicSubnet = ec2.SubnetConfiguration(
-            name="Public", subnet_type=ec2.SubnetType.PUBLIC, cidr_mask=24
-        )
 
-        vpc = ec2.Vpc(
-            self,
-            "YnrVpc",
-            enable_dns_hostnames=False,
-            enable_dns_support=True,
-            create_internet_gateway=True,
-            max_azs=2,
-            nat_gateways=0,
-            subnet_configuration=[publicSubnet],
+        vpc = ec2.Vpc.from_lookup(self, "YnrVpc",
+            vpc_id = ssm.StringParameter.value_from_lookup(self, "/dc/ynr/dev/1/vpcid")
         )
 
         cluster = ecs.Cluster(self, "YnrCluster", vpc=vpc)

commit e7108b1603a8a718d6c556674d73ac4c07586438

deploy: Detect region and account from the env
This fixes the infra test which broke in commit `23f8d486` with the error

```
 RuntimeError: Error: Cannot retrieve value from context provider vpc-provider since account/region are not specified at the stack level. Configure "env" with an account and region when you define your stack.See https://docs.aws.amazon.com/cdk/latest/guide/environments.html for more details.
```

See https://github.com/aws/aws-cdk/issues/3082

deploy/cdk/tests/test_ynr_stack.py
@@ -1,3 +1,5 @@
+import os
+
 import aws_cdk as core
 import aws_cdk.assertions as assertions
 from stack.ynr import YnrStack
@@ -5,7 +7,11 @@ from stack.ynr import YnrStack
 
 def test_template_components():
     app = core.App()
-    stack = YnrStack(app, "YnrStack")
+    stack = YnrStack(app, "YnrStack",
+        env={
+        'account': os.getenv('CDK_DEFAULT_ACCOUNT'),
+        'region': os.getenv('CDK_DEFAULT_REGION')
+    })
     template = assertions.Template.from_stack(stack)
 
     # We're managing the ECS Cluster in-stack, instead of taking a cluster ID