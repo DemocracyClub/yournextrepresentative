1873f0976432b2cc2c35c388c99612359b3b482a
Matt Peperell <mattp@mlemblep.com>
2025-05-24 18:46:16 +0000

    deploy: Use later image
    We've removed the override for entry_point and command since these are now in
    the container definition.

--- deploy/cdk/stack/ynr.py
+++ deploy/cdk/stack/ynr.py
@@ -29,10 +29,8 @@ class YnrStack(Stack):
             desired_count=2,
             task_image_options=ecs_patterns.ApplicationLoadBalancedTaskImageOptions(
                 image=ecs.ContainerImage.from_registry(
-                    "public.ecr.aws/h3q9h5r7/dc-test/ynr:v2"
+                    "public.ecr.aws/h3q9h5r7/dc-test/ynr:v3"
                 ),
-                entry_point=["gunicorn"],
-                command=["--log-level", "debug", "--access-logfile", "-", "--capture-output", "-t", "300", "ynr.wsgi"],
                 # Secrets aren't necessarily "secret", but are created as
                 # environment variables that are looked up at ECS task
                 # instantiation.
