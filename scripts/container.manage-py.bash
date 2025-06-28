12bbbafc8bb3f0b59905715065c225de8ffd6ae4
Matt Peperell <mattp@mlemblep.com>
2025-06-28 21:01:33 +0000

    Fix linting issues on container management scripts

--- scripts/container.manage-py.bash
+++ scripts/container.manage-py.bash
@@ -9,9 +9,9 @@ set -euo pipefail
 
 # The management command does not need to be quoted, unless it contains shell
 # meta-characters. Multiple words are fine.
-mgmtCommand="$@"
+mgmtCommand=$*
 
 # Change to the directory above the directory containing this script.
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/.."
 
-./scripts/container.exec.bash python manage.py $mgmtCommand
+./scripts/container.exec.bash python manage.py "$mgmtCommand"
