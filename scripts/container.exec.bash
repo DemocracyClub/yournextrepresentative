e8bdca2b488521575c8aca8237d2e51f9ba71227
Matt Peperell <mattp@mlemblep.com>
2025-06-28 21:01:33 +0000

    Fix linting issues on container management scripts

--- scripts/container.exec.bash
+++ scripts/container.exec.bash
@@ -16,9 +16,9 @@ set -euo pipefail
 
 # The command being invoked does not need to be quoted, unless it contains
 # shell meta-characters or similar. Multiple words are fine, without quotes.
-command="$@"
+command=$*
 
 # Change to the directory above the directory containing this script.
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/.."
 
-podman compose exec frontend $command
+podman compose exec frontend "$command"
--- scripts/container.image.build.bash
+++ scripts/container.image.build.bash
@@ -4,7 +4,7 @@ set -euo pipefail
 # container.image.build.bash builds an image from a stage defined in
 # container/build/Containerfile, and tags it as "ynr:$image".
 image="$1"; shift
-args="$@"
+args=$*
 
 # Change to the directory above the directory containing this script.
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/.."
@@ -18,4 +18,4 @@ else
 fi; set -u
 
 # Build the image.
-"$builder" build --target "$image" --tag "ynr:$image" -f container/build/Containerfile $args .
+"$builder" build --target "$image" --tag "ynr:$image" -f container/build/Containerfile "$args" .
