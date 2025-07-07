76e7927887ddad365276518909823eebac76816f
Matt Peperell <mattp@mlemblep.com>
2025-07-07 14:57:41 +0000

    cdk: Strip env name from param store paths
    We have a per env per app env store, so the values are inherently dedicated to
    the app instance in question; we don't need path segments indicating the env
    name or app name.

--- cdk/stack/ynr.py
+++ cdk/stack/ynr.py
@@ -26,12 +26,8 @@ class YnrStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        vpc = ec2.Vpc.from_lookup(
-            self,
-            "YnrVpc",
-            vpc_id=ssm.StringParameter.value_from_lookup(
-                self, "/dc/ynr/dev/1/vpcid"
-            ),
+        vpc = ec2.Vpc.from_lookup(self, "YnrVpc",
+            vpc_id = ssm.StringParameter.value_from_lookup(self, "vpcid")
         )
 
         cluster = ecs.Cluster(self, "YnrCluster", vpc=vpc)
@@ -64,35 +60,35 @@ class YnrStack(Stack):
                         ssm.StringParameter.from_string_parameter_name(
                             self,
                             "DSM",
-                            "/dc/ynr/dev/1/web/DJANGO_SETTINGS_MODULE",
+                            "DJANGO_SETTINGS_MODULE",
                         )
                     ),
                     "YNR_AWS_S3_MEDIA_BUCKET": ecs.Secret.from_ssm_parameter(
                         ssm.StringParameter.from_string_parameter_name(
                             self,
                             "MediaBucketName",
-                            "/dc/ynr/dev/1/YNR_AWS_S3_MEDIA_BUCKET",
+                            "YNR_AWS_S3_MEDIA_BUCKET",
                         )
                     ),
                     "YNR_AWS_S3_MEDIA_REGION": ecs.Secret.from_ssm_parameter(
                         ssm.StringParameter.from_string_parameter_name(
                             self,
                             "MediaBucketRegion",
-                            "/dc/ynr/dev/1/YNR_AWS_S3_MEDIA_REGION",
+                            "YNR_AWS_S3_MEDIA_REGION",
                         )
                     ),
                     "YNR_AWS_S3_SOPN_BUCKET": ecs.Secret.from_ssm_parameter(
                         ssm.StringParameter.from_string_parameter_name(
                             self,
                             "SopnBucketName",
-                            "/dc/ynr/dev/1/YNR_AWS_S3_SOPN_BUCKET",
+                            "YNR_AWS_S3_SOPN_BUCKET",
                         )
                     ),
                     "YNR_AWS_S3_SOPN_REGION": ecs.Secret.from_ssm_parameter(
                         ssm.StringParameter.from_string_parameter_name(
                             self,
                             "SopnBucketRegion",
-                            "/dc/ynr/dev/1/YNR_AWS_S3_SOPN_REGION",
+                            "YNR_AWS_S3_SOPN_REGION",
                         )
                     ),
                     "POSTGRES_USERNAME": ecs.Secret.from_ssm_parameter(
@@ -100,7 +96,7 @@ class YnrStack(Stack):
                             self,
                             "DBUSER",
                             encryption_key=encryption_key,
-                            parameter_name="/dc/ynr/dev/1/postgres_username",
+                            parameter_name="postgres_username",
                         )
                     ),
                     "POSTGRES_PASSWORD": ecs.Secret.from_ssm_parameter(
@@ -108,7 +104,7 @@ class YnrStack(Stack):
                             self,
                             "DBPASSWD",
                             encryption_key=encryption_key,
-                            parameter_name="/dc/ynr/dev/1/postgres_password",
+                            parameter_name="postgres_password",
                         )
                     ),
                     "POSTGRES_HOST": ecs.Secret.from_ssm_parameter(
@@ -116,7 +112,7 @@ class YnrStack(Stack):
                             self,
                             "DBHOST",
                             encryption_key=encryption_key,
-                            parameter_name="/dc/ynr/dev/1/postgres_host",
+                            parameter_name="postgres_host",
                         )
                     ),
                 },
