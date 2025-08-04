7c4e6375b3fa5fb6ebae17585e8578a1a439c8ce
Matt Peperell <mattp@mlemblep.com>
2025-08-04 15:33:20 +0000

    Add django-q2 library

--- ynr/settings/base.py
+++ ynr/settings/base.py
@@ -108,6 +108,7 @@ INSTALLED_APPS = (
     "django.contrib.sites",
     "django.contrib.postgres",
     "django_extensions",
+    "django_q",
     "pipeline",
     "sorl.thumbnail",
     "rest_framework",
