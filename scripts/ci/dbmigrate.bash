57c71e07a18b5e692d35205029c2574baf5abfc6
chris48s <git@chris-shaw.dev>
2025-09-18 10:44:25 +0100

    fix manage.py commands, fail build if any command fails

--- scripts/ci/dbmigrate.bash
+++ scripts/ci/dbmigrate.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-set -ex
+set -euxo pipefail
 
 curl "https://s3.amazonaws.com/session-manager-downloads/plugin/latest/ubuntu_64bit/session-manager-plugin.deb" -o "/tmp/session-manager-plugin.deb"
 sudo dpkg -i /tmp/session-manager-plugin.deb
@@ -11,4 +11,43 @@ CLUSTER=$(aws ecs list-clusters | jq -r  .clusterArns[0])
 mapfile -t TASK_LIST < <(aws ecs list-tasks --cluster "${CLUSTER}" | jq -r '.taskArns[]')
 
 # Pick one of the web containers
-aws ecs describe-tasks --include TAGS --cluster "${CLUSTER}" --tasks "${TASK_LIST[@]}" | jq -r '.tasks|map({arn: .containers[].taskArn, role: .overrides.containerOverrides[].name}) | map(select(.role == "web"))[0].arn|split("/") | "unbuffer aws ecs execute-command --cluster " + .[1] + " --command \"python manage.py migrate && python manage.py createcachetable && python manage.py setup_periodic_tasks\" --interactive --task " + .[2]' | sh
+read -r _ CLUSTER_NAME TASK_ID < <(
+  aws ecs describe-tasks --include TAGS \
+    --cluster "${CLUSTER}" \
+    --tasks "${TASK_LIST[@]}" \
+  | jq -r '.tasks
+            | map({arn: .containers[].taskArn, role: .overrides.containerOverrides[].name})
+            | map(select(.role == "web"))[0].arn
+            | split("/") 
+            | @tsv'
+)
+
+# We have to do a bit of a silly dance to see if these jobs worked or not
+# https://github.com/aws/amazon-ecs-agent/issues/2846
+
+MIGRATE_OUTPUT=$(unbuffer aws ecs execute-command --cluster "${CLUSTER_NAME}" --task "${TASK_ID}" --interactive --command "sh -c 'python manage.py migrate && echo SUCCESS'" 2>&1)
+echo "$MIGRATE_OUTPUT"
+if echo "$MIGRATE_OUTPUT" | grep -q 'SUCCESS'; then
+  echo "✔ Migrations applied"
+else
+  echo "✖ Migrations failed"
+  exit 1
+fi
+
+CACHE_OUTPUT=$(unbuffer aws ecs execute-command --cluster "${CLUSTER_NAME}" --task "${TASK_ID}" --interactive --command "sh -c 'python manage.py createcachetable && echo SUCCESS'"2>&1)
+echo "$CACHE_OUTPUT"
+if echo "$CACHE_OUTPUT" | grep -q 'SUCCESS'; then
+    echo "✔ Cache table created"
+else
+    echo "✖ Cache table creation failed"
+    exit 1
+fi
+
+TASKS_OUTPUT=$(unbuffer aws ecs execute-command --cluster "${CLUSTER_NAME}" --task "${TASK_ID}" --interactive --command "sh -c 'python manage.py setup_periodic_tasks && echo SUCCESS'"2>&1)
+echo "$TASKS_OUTPUT"
+if echo "$TASKS_OUTPUT" | grep -q 'SUCCESS'; then
+    echo "✔ Periodic tasks updated"
+else
+    echo "✖ Periodic tasks setup failed"
+    exit 1
+fi
