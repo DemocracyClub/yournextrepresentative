ad19661971de8396099934a1c91bd8ca5c8188c2
chris48s <git@chris-shaw.dev>
2025-10-27 09:12:25 +0000

    implement markdown subset filter

--- ynr/apps/frontend/templatetags/markdown_filter.py
+++ ynr/apps/frontend/templatetags/markdown_filter.py
@@ -2,8 +2,19 @@ import markdown
 import nh3
 from django import template
 from django.utils.safestring import mark_safe
+from markdown_it import MarkdownIt
 
 register = template.Library()
+
+
+"""
+Note:
+We are using two different markdown rendering libraries in this module
+This is for historic reasons rather than because we strictly need to
+
+See
+https://github.com/DemocracyClub/WhoCanIVoteFor/pull/2299#:~:text=Other%20places%20we%20use%20markdown
+"""
 
 
 @register.filter(name="markdown")
@@ -13,3 +24,40 @@ def markdown_filter(text):
 
 
 markdown_filter.is_safe = True
+
+
+@register.filter(name="markdown_subset")
+def markdown_it_filter(text):
+    """
+    note using js-default preset here gives us XSS protection
+    https://markdown-it-py.readthedocs.io/en/latest/security.html
+    so we don't need to run the text through nh3
+    """
+    options = {
+        "breaks": True  # nl2br
+    }
+    renderer = MarkdownIt("js-default", options).disable(
+        [
+            "table",
+            "code",
+            "fence",
+            "blockquote",
+            "backticks",
+            "hr",
+            "reference",
+            "html_block",
+            "heading",
+            "lheading",
+            "linkify",
+            "strikethrough",
+            "link",
+            "image",
+            "autolink",
+            "html_inline",
+        ]
+    )
+    html = renderer.render(text)
+    return mark_safe(html)
+
+
+markdown_it_filter.is_safe = True
--- ynr/apps/frontend/tests/test_markdown_filter.py
+++ ynr/apps/frontend/tests/test_markdown_filter.py
@@ -1,6 +1,12 @@
+from pathlib import Path
+
 import pytest
+from django.test import TestCase
 from django.utils.safestring import SafeString
-from frontend.templatetags.markdown_filter import markdown_filter
+from frontend.templatetags.markdown_filter import (
+    markdown_filter,
+    markdown_it_filter,
+)
 
 
 @pytest.mark.parametrize(
@@ -37,3 +43,81 @@ def test_markdown_filter(input_text, expected_output):
     result = markdown_filter(input_text)
     assert isinstance(result, SafeString)
     assert result == expected_output
+
+
+allowed_markdown_input = """\
+Bla bla bla
+
+- unordered
+- bullet
+- list
+
+**bold** _italic_
+
+1. ordered
+2. bullet
+3. list
+"""
+
+allowed_markdown_expected = """\
+<p>Bla bla bla</p>
+<ul>
+<li>unordered</li>
+<li>bullet</li>
+<li>list</li>
+</ul>
+<p><strong>bold</strong> <em>italic</em></p>
+<ol>
+<li>ordered</li>
+<li>bullet</li>
+<li>list</li>
+</ol>
+"""
+
+
+class TestMarkdownItFilter(TestCase):
+    def test_xss(self):
+        self.assertHTMLEqual(
+            markdown_it_filter('<script>alert("xss")</script>'),
+            "<p>&lt;script&gt;alert(&quot;xss&quot;)&lt;/script&gt;</p>\n",
+        )
+
+    def test_allowed_markdown(self):
+        """
+        Assert that the markdown styles we do want to support i.e:
+        - lists
+        - bold, italic
+        - blockquote
+        render as HTML
+        """
+        self.assertHTMLEqual(
+            markdown_it_filter(allowed_markdown_input),
+            allowed_markdown_expected,
+        )
+
+    def test_forbidden_markdown(self):
+        """
+        Assert that the markdown styles we don't want to support
+        i.e: most of the markdown spec
+        does not get rendered to HTML
+        """
+        test_cases = [
+            "blockquote",
+            "code",
+            "headings",
+            "horizontal_rules",
+            "html",
+            "images",
+            "links",
+            "strikethrough",
+            "tables",
+        ]
+        for test_case in test_cases:
+            with self.subTest(test_case=test_case):
+                input_ = Path(
+                    f"ynr/apps/frontend/tests/forbidden_markdown/input/{test_case}.txt"
+                ).read_text()
+                expected = Path(
+                    f"ynr/apps/frontend/tests/forbidden_markdown/expected/{test_case}.txt"
+                ).read_text()
+                self.assertHTMLEqual(markdown_it_filter(input_), expected)
