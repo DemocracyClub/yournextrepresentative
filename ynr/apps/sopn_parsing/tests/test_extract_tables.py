8424a2c7bb00b058f82ed6b23ef486bcd816d433
symroe <symroe@users.noreply.github.com>
2026-02-09 17:27:44 +0000

    Dummy PDF extractor backend

--- ynr/apps/sopn_parsing/tests/test_extract_tables.py
+++ ynr/apps/sopn_parsing/tests/test_extract_tables.py
@@ -3,8 +3,11 @@ from os.path import abspath, dirname, join
 from candidates.tests.helpers import TmpMediaRootMixin
 from candidates.tests.uk_examples import UK2015ExamplesMixin
 from django.core.files.uploadedfile import SimpleUploadedFile
+from django.core.management import call_command
 from django.test import TestCase
+from django.test.utils import override_settings
 from official_documents.models import BallotSOPN
+from parties.tests.fixtures import DefaultPartyFixtures
 
 
 class TestSOPNHelpers(TmpMediaRootMixin, UK2015ExamplesMixin, TestCase):
@@ -22,3 +25,26 @@ class TestSOPNHelpers(TmpMediaRootMixin, UK2015ExamplesMixin, TestCase):
             uploaded_file=SimpleUploadedFile("sopn.pdf", sopn_file),
             source_url="example.com",
         )
+
+
+class TestDummySOPNParser(DefaultPartyFixtures, UK2015ExamplesMixin, TestCase):
+    @override_settings(USE_DUMMY_PDF_EXTRACTOR=True)
+    def test_basic_parsing(self):
+        doc = BallotSOPN.objects.create(
+            ballot=self.dulwich_post_ballot,
+            source_url="example.com",
+        )
+        self.assertFalse(hasattr(doc, "awstextractparsedsopn"))
+
+        call_command(
+            "sopn_parsing_aws_textract",
+            ballot=self.dulwich_post_ballot.ballot_paper_id,
+            reparse=True,
+            blocking=True,
+            get_results=True,
+            start_analysis=True,
+        )
+        doc.refresh_from_db()
+        # Some smoke tests to see that we've got some data on the model
+        self.assertEqual(doc.awstextractparsedsopn.status, "SUCCEEDED")
+        self.assertEqual(len(doc.awstextractparsedsopn.as_pandas), 4)
