5a3596f362506ba836402d08a207bfa6d8ceeaa0
Sym Roe <sym.roe@talusdesign.co.uk>
2026-04-01 16:47:22 +0100

    CTA for local and prod only
    This will fail on an env without the right DB, so fails stage without it

--- ynr/settings/constants/home_page_cta.py
+++ ynr/settings/constants/home_page_cta.py
@@ -6,28 +6,31 @@ from datetime import date
 # SOPN_TRACKER
 # RESULTS_PROGRESS
 # BY_ELECTIONS
-FRONT_PAGE_CTA = "SOPN_TRACKER"
-SHOW_DATA_DOWNLOAD = False
-SOPN_TRACKER_INFO = {
-    "election_date": "2026-05-07",
-    "election_name": "2026 elections",
-}
-SOPN_SHEET_URL = "https://docs.google.com/spreadsheets/d/1ZpFy1dhmgLSpAhyLcSxsOnLzMH2znlyoS_lgFCjDkqg/edit?gid=0#gid=0"
-SOPN_DATES = [
-    ("Scotland", date(year=2026, month=4, day=1)),
-    ("England and Wales", date(year=2026, month=4, day=9)),
-    # ("England and Wales", date(year=2024, month=4, day=5)),
-    # ("Northern Ireland", date(year=2023, month=4, day=24)),
-    # ("United Kingdom", date(year=2024, month=6, day=7)),
-]
+FRONT_PAGE_CTA = "BY_ELECTIONS"
 
-DATA_DOWNLOAD_INFO = {
-    "election_date": "2026-05-07",
-    "election_name": "2026 local elections",
-    "election_regex": "2026-05-07$",
-}
+if os.environ.get("DC_ENVIRONMENT", "local") in ("local", "production"):
+    FRONT_PAGE_CTA = "SOPN_TRACKER"
+    SHOW_DATA_DOWNLOAD = False
+    SOPN_TRACKER_INFO = {
+        "election_date": "2026-05-07",
+        "election_name": "2026 elections",
+    }
+    SOPN_SHEET_URL = "https://docs.google.com/spreadsheets/d/1ZpFy1dhmgLSpAhyLcSxsOnLzMH2znlyoS_lgFCjDkqg/edit?gid=0#gid=0"
+    SOPN_DATES = [
+        ("Scotland", date(year=2026, month=4, day=1)),
+        ("England and Wales", date(year=2026, month=4, day=9)),
+        # ("England and Wales", date(year=2024, month=4, day=5)),
+        # ("Northern Ireland", date(year=2023, month=4, day=24)),
+        # ("United Kingdom", date(year=2024, month=6, day=7)),
+    ]
 
-SCHEDULED_ELECTION_DATES = ["2026-05-07"]
+    DATA_DOWNLOAD_INFO = {
+        "election_date": "2026-05-07",
+        "election_name": "2026 local elections",
+        "election_regex": "2026-05-07$",
+    }
+
+    SCHEDULED_ELECTION_DATES = ["2026-05-07"]
 
 if os.environ.get("DC_ENVIRONMENT", "local") == "training":
     FRONT_PAGE_CTA = "SOPN_TRACKER"
