315b7f721bea068cbf95042023fc4bdaf8e19625
Sym Roe <sym.roe@talusdesign.co.uk>
2026-04-01 09:49:44 +0100

    2026 election dates

--- ynr/settings/constants/home_page_cta.py
+++ ynr/settings/constants/home_page_cta.py
@@ -6,28 +6,28 @@ from datetime import date
 # SOPN_TRACKER
 # RESULTS_PROGRESS
 # BY_ELECTIONS
-FRONT_PAGE_CTA = "BY_ELECTIONS"
+FRONT_PAGE_CTA = "SOPN_TRACKER"
 SHOW_DATA_DOWNLOAD = False
 SOPN_TRACKER_INFO = {
-    "election_date": "2025-05-01",
-    "election_name": "2025 local elections",
+    "election_date": "2026-05-07",
+    "election_name": "2026 elections",
 }
 SOPN_SHEET_URL = "https://docs.google.com/spreadsheets/d/1SJQdgVe6N4PRoI70XR6XC02MIzchlyB2S3WKRR_GoBw/edit?gid=0#gid=0"
 SOPN_DATES = [
-    # ("Scotland", date(year=2023, month=3, day=30)),
-    ("England", date(year=2025, month=5, day=1)),
+    ("Scotland", date(year=2026, month=4, day=1)),
+    ("England and Wales", date(year=2026, month=4, day=9)),
     # ("England and Wales", date(year=2024, month=4, day=5)),
     # ("Northern Ireland", date(year=2023, month=4, day=24)),
     # ("United Kingdom", date(year=2024, month=6, day=7)),
 ]
 
 DATA_DOWNLOAD_INFO = {
-    "election_date": "2025-05-01",
-    "election_name": "2025 local elections",
-    "election_regex": "local.*.2025-05-01",
+    "election_date": "2026-05-07",
+    "election_name": "2026 local elections",
+    "election_regex": "2026-05-07$",
 }
 
-SCHEDULED_ELECTION_DATES = ["2025-05-01"]
+SCHEDULED_ELECTION_DATES = ["2026-05-07"]
 
 if os.environ.get("DC_ENVIRONMENT", "local") == "training":
     FRONT_PAGE_CTA = "SOPN_TRACKER"
