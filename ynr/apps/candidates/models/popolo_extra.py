4d0f0f516d2a304b72fd485afd04fc744ff65e34
GeoWill <roper.william@gmail.com>
2026-06-18 10:10:10 +0100

    No fullstop at end of ByElectionReason

--- ynr/apps/candidates/models/popolo_extra.py
+++ ynr/apps/candidates/models/popolo_extra.py
@@ -123,23 +123,23 @@ class ByElectionReason(models.TextChoices):
     )
     ELECTED_COUNCIL_MAYOR = (
         "ELECTED_COUNCIL_MAYOR",
-        "The elected member was elected mayor of the council, and cannot hold both positions.",
+        "The elected member was elected mayor of the council, and cannot hold both positions",
     )
     ELECTED_PARLIAMENT = (
         "ELECTED_PARLIAMENT",
-        "The elected member was elected Member of the UK Parliament, and cannot hold both positions.",
+        "The elected member was elected Member of the UK Parliament, and cannot hold both positions",
     )
     ELECTED_STRATEGIC_AUTHORITY_MAYOR = (
         "ELECTED_STRATEGIC_AUTHORITY_MAYOR",
-        "The elected member was elected mayor of a strategic authority, and cannot hold both positions.",
+        "The elected member was elected mayor of a strategic authority, and cannot hold both positions",
     )
     ELECTED_SENEDD = (
         "ELECTED_SENEDD",
-        "The elected member was elected Member of the Senedd, and cannot hold both positions.",
+        "The elected member was elected Member of the Senedd, and cannot hold both positions",
     )
     ELECTED_SCOTTISH_PARLIAMENT = (
         "ELECTED_SCOTTISH_PARLIAMENT",
-        "The elected member was elected Member of the Scottish Parliament, and cannot hold both positions.",
+        "The elected member was elected Member of the Scottish Parliament, and cannot hold both positions",
     )
     OTHER = "OTHER", "Other"
     UNKNOWN = "UNKNOWN", "Unknown"
